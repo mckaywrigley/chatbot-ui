3fecd56d22600bf22861da9fefb321efae416840
Mckay Wrigley <mckaywrigley@gmail.com>
2023-03-18 04:46:42 -0600

    responsive width

--- components/Chat/ChatMessage.tsx
+++ components/Chat/ChatMessage.tsx
@@ -14,7 +14,7 @@ export const ChatMessage: FC<Props> = ({ message, lightMode }) => {
       className={`flex justify-center py-[20px] sm:py-[30px] ${message.role === "assistant" ? "dark:bg-[#444654] dark:text-neutral-100 bg-neutral-100 text-neutral-900 border border-neutral-300 dark:border-none" : "dark:bg-[#343541] dark:text-white text-neutral-900"}`}
       style={{ overflowWrap: "anywhere" }}
     >
-      <div className="w-full sm:w-2/3 md:w-1/2 flex px-4">
+      <div className="w-full sm:w-4/5 md:w-3/5 lg:w-[600px] xl:w-[800px] flex px-4">
         <div className="mr-1 sm:mr-2 font-bold min-w-[40px]">{message.role === "assistant" ? "AI:" : "You:"}</div>
 
         <div className="prose dark:prose-invert mt-[-2px]">
