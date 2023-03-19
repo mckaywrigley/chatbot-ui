891bc38a4589030bd18a4537f861af0e09202f84
Mckay Wrigley <mckaywrigley@gmail.com>
2023-03-18 22:21:11 -0600

    fix loader position

--- components/Chat/ChatLoader.tsx
+++ components/Chat/ChatLoader.tsx
@@ -6,11 +6,11 @@ interface Props {}
 export const ChatLoader: FC<Props> = () => {
   return (
     <div
-      className={`flex justify-center py-[30px] whitespace-pre-wrap dark:bg-[#444654] dark:text-neutral-100 bg-neutral-100 text-neutral-900 dark:border-none"`}
+      className={`flex justify-center py-[20px] sm:py-[30px] whitespace-pre-wrap dark:bg-[#444654] dark:text-neutral-100 bg-neutral-100 text-neutral-900 dark:border-none"`}
       style={{ overflowWrap: "anywhere" }}
     >
-      <div className="w-full px-4 sm:px-0 sm:w-2/3 md:w-1/2 flex">
-        <div className="mr-4 font-bold min-w-[40px]">AI:</div>
+      <div className="w-full sm:w-4/5 md:w-3/5 lg:w-[600px] xl:w-[800px] flex px-4">
+        <div className="mr-1 sm:mr-2 font-bold min-w-[40px]">AI:</div>
         <IconDots className="animate-pulse" />
       </div>
     </div>
