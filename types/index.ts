c0dc3353dca7248d90556d2fba8f86fe2607f5b8
sonnylazuardi <sonnylazuardi@gmail.com>
2023-03-19 22:29:34 +0800

    Fix logic

--- types/index.ts
+++ types/index.ts
@@ -1,13 +1,13 @@
 export enum OpenAIModel {
   GPT_3_5 = "gpt-3.5-turbo",
   GPT_3_5_LEGACY = "gpt-3.5-turbo-0301",
-  GPT_4 = "gpt-4"
+  GPT_4 = "gpt-4",
 }
 
 export const OpenAIModelNames: Record<OpenAIModel, string> = {
   [OpenAIModel.GPT_3_5]: "Default (GPT-3.5)",
-  [OpenAIModel.GPT_3_5_LEGACY]: "Legacy (GPT-3.5)"
-  [OpenAIModel.GPT_4]: "GPT-4"
+  [OpenAIModel.GPT_3_5_LEGACY]: "Legacy (GPT-3.5)",
+  [OpenAIModel.GPT_4]: "GPT-4",
 };
 
 export interface Message {
