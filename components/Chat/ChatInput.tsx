72524aa3c7437e76310bddf79c311f9114e2d5a8
Simon Holmes <srsholmes@gmail.com>
2023-03-22 14:44:35 +0000

    chore add in stop conversation button

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -1,14 +1,17 @@
 import { Message, OpenAIModel, OpenAIModelID } from "@/types";
-import { IconSend } from "@tabler/icons-react";
-import { FC, KeyboardEvent, useEffect, useRef, useState } from "react";
+import { IconSend, IconHandStop} from "@tabler/icons-react";
+import {
+  FC, KeyboardEvent, MutableRefObject, useEffect, useRef, useState
+} from 'react';
 
 interface Props {
   messageIsStreaming: boolean;
   onSend: (message: Message) => void;
   model: OpenAIModel;
+  stopConversationRef: MutableRefObject<boolean>;
 }
 
-export const ChatInput: FC<Props> = ({ onSend, messageIsStreaming, model }) => {
+export const ChatInput: FC<Props> = ({ onSend, messageIsStreaming, model, stopConversationRef }) => {
   const [content, setContent] = useState<string>();
   const [isTyping, setIsTyping] = useState<boolean>(false);
 
@@ -67,36 +70,51 @@ export const ChatInput: FC<Props> = ({ onSend, messageIsStreaming, model }) => {
     }
   }, [content]);
 
+  function handleStopConversation(){
+    stopConversationRef.current = true;
+    setTimeout(() => {
+      stopConversationRef.current = false;
+    }, 1000);
+  }
+
   return (
     <div className="absolute bottom-0 left-0 w-full border-t md:border-t-0 dark:border-white/20 md:border-transparent md:dark:border-transparent dark:bg-[#444654] md:dark:bg-gradient-to-t from-[#343541] via-[#343541] to-[#343541]/0 bg-white md:dark:!bg-transparent dark:md:bg-vert-dark-gradient pt-2">
       <div className="stretch mx-2 md:mt-[52px] mt-4 flex flex-row gap-3 last:mb-2 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-3xl">
           <div className="flex flex-col w-full py-2 flex-grow md:py-3 md:pl-4 relative border border-black/10 bg-white dark:border-gray-900/50 dark:text-white dark:bg-[#40414F] rounded-md shadow-[0_0_10px_rgba(0,0,0,0.10)] dark:shadow-[0_0_15px_rgba(0,0,0,0.10)]">
-      <textarea
-        ref={textareaRef}
-        className="text-base m-0 w-full resize-none outline-none border-0 bg-transparent p-0 pr-7 focus:ring-0 focus-visible:ring-0 dark:bg-transparent pl-2 md:pl-0"
-        style={{
-          resize: "none",
-          bottom: `${textareaRef?.current?.scrollHeight}px`,
-          maxHeight: "400px",
-          overflow: `${textareaRef.current && textareaRef.current.scrollHeight > 400 ? "auto" : "hidden"}`
-        }}
-        placeholder="Type a message..."
-        value={content}
-        rows={1}
-        onCompositionStart={() => setIsTyping(true)}
-        onCompositionEnd={() => setIsTyping(false)}
-        onChange={handleChange}
-        onKeyDown={handleKeyDown}
-      />
+            <textarea
+              ref={textareaRef}
+              className="text-base m-0 w-full resize-none outline-none border-0 bg-transparent p-0 pr-7 focus:ring-0 focus-visible:ring-0 dark:bg-transparent pl-2 md:pl-0"
+              style={{
+                resize: "none",
+                bottom: `${textareaRef?.current?.scrollHeight}px`,
+                maxHeight: "400px",
+                overflow: `${textareaRef.current && textareaRef.current.scrollHeight > 400 ? "auto" : "hidden"}`
+              }}
+              placeholder="Type a message..."
+              value={content}
+              rows={1}
+              onCompositionStart={() => setIsTyping(true)}
+              onCompositionEnd={() => setIsTyping(false)}
+              onChange={handleChange}
+              onKeyDown={handleKeyDown}
+            />
 
-      <button
-        className="absolute right-5 focus:outline-none text-neutral-800 hover:text-neutral-900 dark:text-neutral-100 dark:hover:text-neutral-200 dark:bg-opacity-50 hover:bg-neutral-200 p-1 rounded-sm"
-        onClick={handleSend}
-      >
-        <IconSend size={16} className="opacity-60"/>
-      </button>
-      </div>
-      </div>
+            <button
+              className="absolute right-5 focus:outline-none text-neutral-800 hover:text-neutral-900 dark:text-neutral-100 dark:hover:text-neutral-200 dark:bg-opacity-50 hover:bg-neutral-200 p-1 rounded-sm"
+              onClick={handleSend}
+            >
+              <IconSend size={16} className="opacity-60"/>
+            </button>
+            {messageIsStreaming ? (
+              <button
+                className="absolute right-12 focus:outline-none text-neutral-800 hover:text-neutral-900 dark:text-neutral-100 dark:hover:text-neutral-200 dark:bg-opacity-50 hover:bg-neutral-200 p-1 rounded-sm"
+                onClick={handleStopConversation}
+              >
+                <IconHandStop size={16} className="opacity-60"/>
+              </button>
+            ): null}
+          </div>
+        </div>
       <div className="px-3 pt-2 pb-3 text-center text-xs text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6"><a href="https://github.com/mckaywrigley/chatbot-ui" target="_blank" rel="noreferrer" className="underline">ChatBot UI</a>. Chatbot UI is an advanced chatbot kit for OpenAI&apos;s chat models aiming to mimic ChatGPT&apos;s interface and functionality.</div>
     </div>
   );
