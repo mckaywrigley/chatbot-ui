2b1ef7be3e2e8f040d4c2b7db6774eeb220b568e
Awesh Choudhary <66569398+aweshchoudhary@users.noreply.github.com>
2023-03-23 20:01:45 +0530

    SEO Fixed and Added Semantic Html Tags (#98)

--- components/Mobile/Navbar.tsx
+++ components/Mobile/Navbar.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 export const Navbar: FC<Props> = ({ selectedConversation, onNewConversation }) => {
   return (
-    <div className="flex justify-between bg-[#202123] py-3 px-4 w-full">
+    <nav className="flex justify-between bg-[#202123] py-3 px-4 w-full">
       <div className="mr-4"></div>
 
       <div className="max-w-[240px] whitespace-nowrap overflow-hidden text-ellipsis">{selectedConversation.name}</div>
@@ -18,6 +18,6 @@ export const Navbar: FC<Props> = ({ selectedConversation, onNewConversation }) =
         className="cursor-pointer hover:text-neutral-400"
         onClick={onNewConversation}
       />
-    </div>
+    </nav>
   );
 };
