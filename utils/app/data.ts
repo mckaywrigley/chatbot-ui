a1a8ac42a6d7f8b000af78df68c86d96d1229331
patanjalikr13 <42902972+patanjalikr13@users.noreply.github.com>
2023-03-23 12:46:34 +0530

    added download button, downlaod handler and necessary utility functions (#85)
    Co-authored-by: Patanjali Kumar <patanjali@oddup.com>

--- utils/app/data.ts
+++ utils/app/data.ts
@@ -21,3 +21,44 @@ export const importConversations = (conversations: Conversation[]) => {
   localStorage.setItem("conversationHistory", JSON.stringify(conversations));
   localStorage.setItem("selectedConversation", JSON.stringify(conversations[conversations.length - 1]));
 };
+
+interface languageMap {
+  [key: string]: string | undefined
+}
+
+export const programmingLanguages: languageMap = {
+  'javascript': '.js',
+  'python': '.py',
+  'java': '.java',
+  'c': '.c',
+  'cpp': '.cpp',
+  'c++': '.cpp',
+  'c#': '.cs',
+  'ruby': '.rb',
+  'php': '.php',
+  'swift': '.swift',
+  'objective-c': '.m',
+  'kotlin': '.kt',
+  'typescript': '.ts',
+  'go': '.go',
+  'perl': '.pl',
+  'rust': '.rs',
+  'scala': '.scala',
+  'haskell': '.hs',
+  'lua': '.lua',
+  'shell': '.sh',
+  'sql': '.sql',
+  'html': '.html',
+  'css': '.css'
+  // add more file extensions here, make sure the key is same as language prop in CodeBlock.tsx component
+};
+
+
+export const generateRandomString = (length: Number, lowercase=false) => {
+  const chars = "ABCDEFGHJKLMNPQRSTUVWXY3456789"; // excluding similar looking characters like Z, 2, I, 1, O, 0
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return lowercase ? result.toLowerCase() : result;
+}
