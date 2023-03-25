a0b9425b4d9f4d76589247a8ef906e1f77b3106a
Jungley Yeh <yejing@live.cn>
2023-03-25 23:07:28 +0800

    fix: locale not working in Docker environment

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,12 @@
+const path = require("path");
+
 module.exports = {
-    i18n: {
-      defaultLocale: 'en',
-      locales: ['en', 'zh'],
-    },
-  }
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "zh"],
+  },
+  localePath:
+    typeof window === "undefined"
+      ? require("path").resolve("./public/locales")
+      : "/public/locales",
+};
