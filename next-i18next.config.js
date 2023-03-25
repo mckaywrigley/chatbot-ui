a0a2cb8b35489120d37215c1a01b9edb88162b78
TULASEE RAO CHINTHA <tulasi.chintha@gmail.com>
2023-03-25 17:35:29 -0400

    Added Telugu language(South Indian) translations (#161)

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   i18n: {
     defaultLocale: "en",
-    locales: ["de", "en", "fr", "ru", "zh", "es", "sv", "pt"],
+    locales: ["de", "en", "fr", "ru", "zh", "es", "sv", "pt", "te"],
   },
   localePath:
     typeof window === "undefined"
