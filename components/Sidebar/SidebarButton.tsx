81ee8782a314155735d292bca4a58245f7bfac3f
bullman <brad@ullman.com>
2023-03-24 18:05:10 -0700

    update sidebar elements to be buttons so they are keyboard navigatable

--- components/Sidebar/SidebarButton.tsx
+++ components/Sidebar/SidebarButton.tsx
@@ -8,12 +8,12 @@ interface Props {
 
 export const SidebarButton: FC<Props> = ({ text, icon, onClick }) => {
   return (
-    <div
+    <button
       className="flex py-3 px-3 gap-3 rounded-md hover:bg-gray-500/10 transition-colors duration-200 text-white cursor-pointer w-full items-center"
       onClick={onClick}
     >
       <div>{icon}</div>
       <div>{text}</div>
-    </div>
+    </button>
   );
 };
