cb58a703e331467db2ef696cee319d913082c336
Danil Shishkevich <44939683+shishkevichd@users.noreply.github.com>
2023-03-25 23:33:29 +0700

    feat: Russian language (#148)
    * feat: russian (Русский) language
    
    * chore: correct translate
    
    * chore: correct "System prompt" translate

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   i18n: {
     defaultLocale: "en",
-    locales: ["en", "zh"],
+    locales: ["en", "zh", "ru"],
   },
   localePath:
     typeof window === "undefined"
