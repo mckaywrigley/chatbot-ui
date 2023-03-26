b137d6f993f29d655599bd0b49d05e5f5aeda341
Danil Shishkevich <danilshishkevich@gmail.com>
2023-03-26 17:58:03 +0700

    chore: stylize home modal chore: add loading spinner

--- components/Chat/ModelSelect.tsx
+++ components/Chat/ModelSelect.tsx
@@ -1,6 +1,7 @@
 import { OpenAIModel } from '@/types';
 import { FC } from 'react';
 import { useTranslation } from 'next-i18next';
+import { IconCaretDown } from '@tabler/icons-react';
 
 interface Props {
   model: OpenAIModel;
@@ -10,27 +11,33 @@ interface Props {
 
 export const ModelSelect: FC<Props> = ({ model, models, onModelChange }) => {
   const { t } = useTranslation('chat');
+
   return (
     <div className="flex flex-col">
       <label className="mb-2 text-left text-neutral-700 dark:text-neutral-400">
         {t('Model')}
       </label>
-      <select
-        className="focus:shadow-outline w-full cursor-pointer appearance-none rounded-lg border border-neutral-500 p-3 text-neutral-900 dark:bg-[#343541] dark:text-white"
-        placeholder={t('Select a model') || ''}
-        value={model.id}
-        onChange={(e) => {
-          onModelChange(
-            models.find((model) => model.id === e.target.value) as OpenAIModel,
-          );
-        }}
-      >
-        {models.map((model) => (
-          <option key={model.id} value={model.id}>
-            {model.name}
-          </option>
-        ))}
-      </select>
+      <div className="flex items-center w-full rounded-lg border border-neutral-200 text-neutral-900 dark:border-neutral-600 dark:bg-[#343541] dark:text-white">
+        <select
+          id='modelSelect'
+          className='border-0 bg-transparent mr-3 outline-0 p-2 cursor-pointer w-full'
+          placeholder={t('Select a model') || ''}
+          value={model.id}
+          onChange={(e) => {
+            onModelChange(
+              models.find(
+                (model) => model.id === e.target.value,
+              ) as OpenAIModel,
+            );
+          }}
+        >
+          {models.map((model) => (
+            <option key={model.id} value={model.id}>
+              {model.name}
+            </option>
+          ))}
+        </select>
+      </div>
     </div>
   );
 };
---
+++ components/Global/Spinner.tsx
@@ -0,0 +1,32 @@
+import { FC } from 'react';
+
+interface Props {
+  size?: string;
+  className?: string;
+}
+
+export const Spinner: FC<Props> = ({ className, size = '1em' }) => {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="none"
+      stroke-width="2"
+      viewBox="0 0 24 24"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className={`animate-spin ${className}`}
+      height={size}
+      width={size}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <line x1="12" y1="2" x2="12" y2="6"></line>
+      <line x1="12" y1="18" x2="12" y2="22"></line>
+      <line x1="4.93" y1="4.93" x2="7.76" y2="7.76"></line>
+      <line x1="16.24" y1="16.24" x2="19.07" y2="19.07"></line>
+      <line x1="2" y1="12" x2="6" y2="12"></line>
+      <line x1="18" y1="12" x2="22" y2="12"></line>
+      <line x1="4.93" y1="19.07" x2="7.76" y2="16.24"></line>
+      <line x1="16.24" y1="7.76" x2="19.07" y2="4.93"></line>
+    </svg>
+  );
+};
