23f27e40de59c8cdb36b0a51ab8775add6fe1c91
Jungley Yeh <yejing@live.cn>
2023-03-28 02:44:45 +0800

    feat: disable pwa in development

--- next.config.js
+++ next.config.js
@@ -19,7 +19,7 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   // register: true,
   // skipWaiting: true,
-  // disable:process.env.NODE_ENV === 'development'
+  disable: process.env.NODE_ENV === 'development',
 });
 
 module.exports = withPWA(nextConfig);
