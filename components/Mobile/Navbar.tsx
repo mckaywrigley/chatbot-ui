7a78a298a5fe2999ac4ed05618f280478e61b4eb
Syed Muzamil <38078427+SyedMuzamilM@users.noreply.github.com>
2023-03-27 22:00:00 +0530

    fix: overlap between plus and prompt menu icon (#230)

--- components/Mobile/Navbar.tsx
+++ components/Mobile/Navbar.tsx
@@ -20,7 +20,7 @@ export const Navbar: FC<Props> = ({
       </div>
 
       <IconPlus
-        className="cursor-pointer hover:text-neutral-400"
+        className="cursor-pointer hover:text-neutral-400 mr-8"
         onClick={onNewConversation}
       />
     </nav>
