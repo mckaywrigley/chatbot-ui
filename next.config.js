0263fdb65f204c88332a954e114fb0654d914f50
Jungley Yeh <yejing@live.cn>
2023-03-28 02:00:10 +0800

    feat: support pwa

--- next.config.js
+++ next.config.js
@@ -15,4 +15,11 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  // register: true,
+  // skipWaiting: true,
+  // disable:process.env.NODE_ENV === 'development'
+});
+
+module.exports = withPWA(nextConfig);
