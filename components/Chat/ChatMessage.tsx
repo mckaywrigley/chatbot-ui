0b81918a5ebaea8de2e4f30b3bbafe91f16a1d34
bullman <brad@ullman.com>
2023-03-27 20:53:30 -0700

    align message role description

--- components/Chat/ChatMessage.tsx
+++ components/Chat/ChatMessage.tsx
@@ -80,7 +80,7 @@ export const ChatMessage: FC<Props> = memo(
         style={{ overflowWrap: 'anywhere' }}
       >
         <div className="relative m-auto flex gap-4 p-4 text-base md:max-w-2xl md:gap-6 md:py-6 lg:max-w-2xl lg:px-0 xl:max-w-3xl">
-          <div className="min-w-[40px] font-bold">
+          <div className="min-w-[40px] font-bold text-right">
             {message.role === 'assistant' ? t('AI') : t('You')}:
           </div>
 
