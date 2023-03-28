feef5540d6b74d685b68dd498ea1b52860078b35
ChenZhaoYu <790348264@qq.com>
2023-03-28 09:29:14 +0800

    chore: update locales

--- components/Promptbar/Promptbar.tsx
+++ components/Promptbar/Promptbar.tsx
@@ -111,14 +111,14 @@ export const Promptbar: FC<Props> = ({
         </button>
 
         <button
-          className="ml-2 flex flex-shrink-0 cursor-pointer items-center gap-3 rounded-md border border-white/20 p-3 text-sm text-white transition-colors duration-200 hover:bg-gray-500/10"
+          className="flex items-center flex-shrink-0 gap-3 p-3 ml-2 text-sm text-white transition-colors duration-200 border rounded-md cursor-pointer border-white/20 hover:bg-gray-500/10"
           onClick={() => onCreateFolder(t('New folder'))}
         >
           <IconFolderPlus size={16} />
         </button>
 
         <IconArrowBarRight
-          className="ml-1 hidden cursor-pointer p-1 text-neutral-300 hover:text-neutral-400 sm:flex"
+          className="hidden p-1 ml-1 cursor-pointer text-neutral-300 hover:text-neutral-400 sm:flex"
           size={32}
           onClick={onToggleSidebar}
         />
@@ -126,7 +126,7 @@ export const Promptbar: FC<Props> = ({
 
       {prompts.length > 1 && (
         <Search
-          placeholder="Search prompts..."
+          placeholder={t('Search prompts...') || ''}
           searchTerm={searchTerm}
           onSearch={setSearchTerm}
         />
@@ -134,7 +134,7 @@ export const Promptbar: FC<Props> = ({
 
       <div className="flex-grow overflow-auto">
         {folders.length > 0 && (
-          <div className="flex border-b border-white/20 pb-2">
+          <div className="flex pb-2 border-b border-white/20">
             <PromptFolders
               searchTerm={searchTerm}
               prompts={filteredPrompts}
