c5b89d5f16c8792c8397508a3b9affcc66babc7e
Jack wu <jackwu833016@gmail.com>
2023-03-30 09:59:38 -0400

    Add Vercel tracker to gather app usages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,14 @@ import '@/styles/globals.css';
 import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
-
+import { Analytics } from '@vercel/analytics/react';
 const inter = Inter({ subsets: ['latin'] });
 
 function App({ Component, pageProps }: AppProps<{}>) {
   return (
     <main className={inter.className}>
       <Component {...pageProps} />
+      <Analytics />
     </main>
   );
 }
