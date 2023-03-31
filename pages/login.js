a2c760a8c8e736ae711601deedeba58ad9f01847
seichris <cerujurasa@gmail.com>
2023-03-31 11:59:14 +0400

    pw protect - fix login design

--- pages/login.js
+++ pages/login.js
@@ -21,37 +21,50 @@ export default function LoginPage() {
 
     if (response.ok) {
       setError(null);
-      router.push('/protected');
+      router.push('/');
     } else {
       setError('Invalid credentials');
     }
   };
 
   return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Username:
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>
-        <br />
-        <button type="submit">Login</button>
-      </form>
-      {error && <p>{error}</p>}
-    </div>
+    <div className="p-10">
+    <h1 className="text-3xl font-medium mb-5 text-gray-400">8o.vc</h1>
+    <form onSubmit={handleSubmit}>
+      <label className="block mb-2">
+        <input
+          className="w-full border-2 border-gray-400 p-2 rounded-md focus:outline-none focus:border-blue-500"
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </label>
+      <label className="block mb-2">
+        <input
+          className="w-full border-2 border-gray-400 p-2 rounded-md focus:outline-none focus:border-blue-500"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <button type="submit" className="relative inline-flex items-center justify-center px-4 py-2 overflow-hidden font-bold text-white rounded-md shadow-2xl group">
+        <span className="absolute inset-0 w-full h-full transition duration-300 ease-out opacity-0 bg-gradient-to-br from-pink-600 via-purple-700 to-blue-400 group-hover:opacity-100"></span>
+        {/* Top glass gradient */}
+        <span className="absolute top-0 left-0 w-full bg-gradient-to-b from-white to-transparent opacity-5 h-1/3"></span>
+        {/* Bottom gradient */}
+        <span className="absolute bottom-0 left-0 w-full h-1/3 bg-gradient-to-t from-white to-transparent opacity-5"></span>
+        {/* Left gradient */}
+        <span className="absolute bottom-0 left-0 w-4 h-full bg-gradient-to-r from-white to-transparent opacity-5"></span>
+        {/* Right gradient */}
+        <span className="absolute bottom-0 right-0 w-4 h-full bg-gradient-to-l from-white to-transparent opacity-5"></span>
+        <span className="absolute inset-0 w-full h-full border border-white rounded-md opacity-10"></span>
+        <span className="absolute w-0 h-0 transition-all duration-300 ease-out bg-white rounded-full group-hover:w-56 group-hover:h-56 opacity-5"></span>
+        <span className="relative">Login</span>
+      </button>
+    </form>
+    {error && <p className="text-red-400 mt-3">{error}</p>}
+  </div>
   );
 }
