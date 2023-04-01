65f01eeb22130e125830ac90a767a64abd20f4cb
Jack wu <jackwu833016@gmail.com>
2023-04-01 01:40:25 -0400

    Set promp sidebar to be closed by default

--- pages/index.tsx
+++ pages/index.tsx
@@ -67,7 +67,7 @@ const Home: React.FC<HomeProps> = ({
   const [showSidebar, setShowSidebar] = useState<boolean>(true);
 
   const [prompts, setPrompts] = useState<Prompt[]>([]);
-  const [showPromptbar, setShowPromptbar] = useState<boolean>(true);
+  const [showPromptbar, setShowPromptbar] = useState<boolean>(false);
 
   // REFS ----------------------------------------------
 
