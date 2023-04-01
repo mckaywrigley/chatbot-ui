d1c82548a8b5556e71d62182ad3f6172c21e7b55
Nguyen K <53246028+k9ele7en@users.noreply.github.com>
2023-04-01 07:11:40 +0000

    add VN flag icon, remove text about UI below txtbox

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -311,20 +311,6 @@ export const ChatInput: FC<Props> = ({
           )}
         </div>
       </div>
-      <div className="px-3 pt-2 pb-3 text-center text-[12px] text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6">
-        <a
-          href="https://github.com/mckaywrigley/chatbot-ui"
-          target="_blank"
-          rel="noreferrer"
-          className="underline"
-        >
-          ChatBot UI
-        </a>
-        .{' '}
-        {t(
-          "Chatbot UI is an advanced chatbot kit for OpenAI's chat models aiming to mimic ChatGPT's interface and functionality.",
-        )}
-      </div>
     </div>
   );
 };
