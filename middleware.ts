96e8798ed38b1f74150c8cc9583f54868b3f4152
Nguyen K <53246028+k9ele7en@users.noreply.github.com>
2023-04-01 09:47:28 +0000

    test

---
+++ middleware.ts
@@ -0,0 +1,25 @@
+import type { NextRequest } from 'next/server'
+
+import { NextResponse } from 'next/server'
+
+// Block Austria, prefer Germany
+const ALLOWED_COUNTRY = 'VN'
+
+// Limit middleware pathname config
+export const config = {
+  matcher: '/',
+}
+
+export function middleware(req: NextRequest) {
+  // Extract country
+  const country = req.geo.country || 'US'
+  // Specify the correct pathname
+  if (country === ALLOWED_COUNTRY) {
+      req.nextUrl.pathname = `/`
+} else {
+      req.nextUrl.pathname = '/blocked'
+  }
+
+  // Rewrite to URL
+  return NextResponse.rewrite(req.nextUrl)
+}
---
+++ pages/blocked.tsx
@@ -0,0 +1,3 @@
+export default function Custom404() {
+    return <h1 className="text-white">404 - Page Not Found</h1>
+  }
