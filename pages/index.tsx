d19dc086e3beecfa8568e23da7f249d6e00a9898
Nguyễn Việt Bắc <86348885+egochao@users.noreply.github.com>
2023-04-01 04:43:42 +0000

    Update title

--- pages/index.tsx
+++ pages/index.tsx
@@ -625,7 +625,7 @@ const Home: React.FC<HomeProps> = ({
   return (
     <>
       <Head>
-        <title>Chatbot UI</title>
+        <title>AI tutor</title>
         <meta name="description" content="ChatGPT but better." />
         <meta
           name="viewport"
