21f485d4e1486df3fcd5a265b1737ae7544adb54
Jungley Yeh <yejing@live.cn>
2023-04-03 22:12:45 +0800

    chore: add husky and lint-staged

---
+++ .lintstagedrc.js
@@ -0,0 +1,10 @@
+const path = require('path')
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`
+
+module.exports = {
+  '*.{js,jsx,ts,tsx}': ["prettier --write", buildEslintCommand],
+}
