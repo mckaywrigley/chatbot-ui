2df9605af2f3a7a6a94e590add2877a4420e343a
Steven Tsao <steventsoa@me.com>
2023-04-03 13:43:10 -0700

    format

--- utils/app/prompt.ts
+++ utils/app/prompt.ts
@@ -2,13 +2,16 @@ import { OpenAIModel } from '@/types/openai';
 import { Prompt } from '@/types/prompt';
 import { v4 as uuidv4 } from 'uuid';
 
-export const createPrompt = (promptName: string, model: OpenAIModel): Prompt => {
-    return {
-      id: uuidv4(),
-      name: promptName,
-      description: '',
-      content: '',
-      model,
-      folderId: null,
-    }
+export const createPrompt = (
+  promptName: string,
+  model: OpenAIModel,
+): Prompt => {
+  return {
+    id: uuidv4(),
+    name: promptName,
+    description: '',
+    content: '',
+    model,
+    folderId: null,
   };
+};
