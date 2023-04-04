ff87eceba66ecb232b068de88ac88abb23dd53ee
Will Turner <rwturner17@gmail.com>
2023-04-03 20:25:11 -0400

    add turtlestream

--- utils/server/index.ts
+++ utils/server/index.ts
@@ -5,7 +5,7 @@ import {
   ParsedEvent,
   ReconnectInterval,
 } from 'eventsource-parser';
-import { OPENAI_API_HOST } from '../app/const';
+import { OPENAI_API_HOST, TURTLE_HOST } from '../app/const';
 
 export class OpenAIError extends Error {
   type: string;
@@ -20,6 +20,56 @@ export class OpenAIError extends Error {
     this.code = code;
   }
 }
+
+
+export const TurtleStream = async (
+  model: OpenAIModel,
+  systemPrompt: string,
+  key: string,
+  messages: Message[],
+) => {
+  const chatBody = JSON.stringify({
+    model: model.id,
+    messages: [
+      {
+        role: 'system',
+        content: systemPrompt,
+      },
+      ...messages,
+    ]
+  })
+  console.log(chatBody)
+  const res = await fetch(`${TURTLE_HOST}/v1/chat/completions`, {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${key ? key : process.env.OPENAI_API_KEY}`,
+      ...(process.env.OPENAI_ORGANIZATION && {
+        'OpenAI-Organization': process.env.OPENAI_ORGANIZATION,
+      }),
+    },
+    method: 'POST',
+    body: chatBody
+  });
+
+  if (!res.ok) {
+    throw new Error(`Fetch failed with status ${res.status}`);
+  }
+
+  const json = await res.json();
+  const text = json.choices[0].message.content;
+
+  const encoder = new TextEncoder();
+
+  const stream = new ReadableStream({
+    start(controller) {
+      controller.enqueue(encoder.encode(text));
+      controller.close();
+    },
+  });
+
+  return stream;
+};
+
 
 export const OpenAIStream = async (
   model: OpenAIModel,
