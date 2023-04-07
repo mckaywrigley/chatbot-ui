b3fcaf44fee3a7d6229136275deb916c01ef7669
Jack wu <jackwu833016@gmail.com>
2023-04-07 15:27:06 -0400

    Remove chat loader message

--- components/Chat/ChatLoader.tsx
+++ components/Chat/ChatLoader.tsx
@@ -10,7 +10,6 @@ export const ChatLoader: FC<Props> = () => {
       style={{ overflowWrap: 'anywhere' }}
     >
       <div className="m-auto flex gap-4 p-4 text-base md:max-w-2xl md:gap-6 md:py-6 lg:max-w-2xl lg:px-0 xl:max-w-3xl">
-        <div className="min-w-[40px] text-right font-bold">AI:</div>
         <IconDots className="animate-pulse" />
       </div>
     </div>
