3c75ca1aca33ee160035a175aa86c4a924071578
Jack wu <jackwu833016@gmail.com>
2023-04-08 00:18:49 -0400

    Add conversation share button to mobile navbar

--- components/Mobile/Navbar.tsx
+++ components/Mobile/Navbar.tsx
@@ -1,6 +1,7 @@
 import { Conversation } from '@/types/chat';
 import { IconPlus } from '@tabler/icons-react';
 import { FC } from 'react';
+import { StoreConversationButton } from '../Global/StoreConversationButton';
 
 interface Props {
   selectedConversation: Conversation;
@@ -12,15 +13,17 @@ export const Navbar: FC<Props> = ({
   onNewConversation,
 }) => {
   return (
-    <nav className="flex w-full justify-between bg-[#202123] py-3 px-4">
-      <div className="mr-4"></div>
+    <nav className="flex w-full items-center justify-between bg-[#202123] py-3 px-4">
+      <div className="mr-4 pl-8">
+        <StoreConversationButton conversation={selectedConversation} />
+      </div>
 
       <div className="max-w-[240px] overflow-hidden text-ellipsis whitespace-nowrap">
         {selectedConversation.name}
       </div>
 
       <IconPlus
-        className="cursor-pointer hover:text-neutral-400 mr-8"
+        className="mr-10 ml-2 cursor-pointer hover:text-neutral-400"
         onClick={onNewConversation}
       />
     </nav>
