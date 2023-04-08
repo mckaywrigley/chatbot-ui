9c3e03a9414e6e42137807dd56bf6314c47891e3
Jack wu <jackwu833016@gmail.com>
2023-04-08 13:09:22 -0400

    Work around clipboard access on mobile devices

--- components/Global/StoreConversationButton.tsx
+++ components/Global/StoreConversationButton.tsx
@@ -12,6 +12,16 @@ export const StoreConversationButton: FC<Props> = ({ conversation }) => {
   const { t } = useTranslation('chat');
   const [loading, setLoading] = useState(false);
 
+  const shareConversationCopyButtonOnClick = async (url: string) => {
+    try {
+      await navigator.clipboard.writeText(url);
+      toast.success('Link copied to clipboard!');
+      toast.dismiss('store-conversation-toast');
+    } catch (error) {
+      toast.error('Failed to copy the link to clipboard');
+    }
+  };
+
   const storeConversation = async () => {
     const confirmed = window.confirm(
       t(
@@ -45,8 +55,29 @@ export const StoreConversationButton: FC<Props> = ({ conversation }) => {
       const { accessible_id } = await response.json();
 
       const url = `${window.location.origin}?shareable_conversation_id=${accessible_id}`;
-      await navigator.clipboard.writeText(url);
-      toast.success(t(`Conversation saved! The sharable link is copied to clipboard.`));
+
+      if (!navigator.clipboard || !navigator.clipboard.writeText) {
+        toast.error(t(`Failed to copy the sharable link to clipboard.`));
+      } else {
+        toast(
+          <div>
+            <span>Conversation saved!</span>
+            <button
+              onClick={() => {
+                shareConversationCopyButtonOnClick(url);
+              }}
+              className="mt-2 m-auto block rounded-md bg-blue-500 px-2 py-1 text-white text-sm"
+            >
+              Copy link
+            </button>
+          </div>,
+          {
+            id: 'store-conversation-toast',
+            icon: '🔗',
+            duration: 8000,
+          },
+        );
+      }
     } catch (error) {
       toast.error((error as any).message);
     }
