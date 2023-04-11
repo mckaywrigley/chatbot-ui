7d539ee58bbbbff1258dbd4fce2f7c3f643243de
Filippo Pedrazzini <filippo.pedrazzini@joandko.io>
2023-04-11 23:37:28 +0200

    fixed sidecar running

--- package_app.sh
+++ package_app.sh
@@ -1,9 +1,9 @@
-sudo lsof -i -P | grep LISTEN | grep :8002
+sudo lsof -i -P | grep LISTEN | grep :8001
 
 lsof -P | grep ':8002' | awk '{print $2}' | xargs kill -9
 lsof -P | grep ':8001' | awk '{print $2}' | xargs kill -9
 
-wget https://github.com/PremAI-Org/the-ai-box/releases/download/v0.0.14/binaries-macOS.zip
+wget https://github.com/PremAI-Org/the-ai-box/releases/download/v0.0.19/binaries-macOS.zip
 mv binaries-macOS.zip ./src-tauri/binaries/binaries-macOS.zip
 unzip ./src-tauri/binaries/binaries-macOS.zip -d ./src-tauri/binaries/
 
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,6 +4,10 @@
 )]
 
 use std::{env, net::TcpStream, thread, time::Duration};
+use tauri::{
+  api::process::{Command, CommandEvent},
+  Manager,
+};
 
 fn wait_for_api(port: u16) {
   loop {
@@ -18,10 +22,34 @@ fn wait_for_api(port: u16) {
 }
 
 fn main() {
-
-  wait_for_api(8001);
-
   tauri::Builder::default()
+    .setup(|app| {
+      let window = app.get_window("main").unwrap();
+      tauri::async_runtime::spawn(async move {
+        let (mut rx, mut child) = Command::new_sidecar("apis")
+          .expect("failed to setup `apis` sidecar")
+          .spawn()
+          .expect("Failed to spawn packaged python.");
+
+        let mut i = 0;
+        while let Some(event) = rx.recv().await {
+          if let CommandEvent::Stdout(line) = event {
+            window
+              .emit("message", Some(format!("'{}'", line)))
+              .expect("failed to emit event");
+            i += 1;
+            if i == 4 {
+              child.write("message from Rust\n".as_bytes()).unwrap();
+              i = 0;
+            }
+          }
+        }
+      });
+      
+      wait_for_api(8001);
+
+      Ok(())
+    })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
