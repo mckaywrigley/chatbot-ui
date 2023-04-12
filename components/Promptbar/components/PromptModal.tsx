865b7d57d088438517eac37e6a85d4023c247daa
Syed Muzamil <smmhd121@gmail.com>
2023-04-12 23:50:05 +0530

    fix: overlap between the modal and icons

--- components/Promptbar/components/PromptModal.tsx
+++ components/Promptbar/components/PromptModal.tsx
@@ -51,7 +51,7 @@ export const PromptModal: FC<Props> = ({ prompt, onClose, onUpdatePrompt }) => {
 
   return (
     <div
-      className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-100"
+      className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
       onKeyDown={handleEnter}
     >
       <div className="fixed inset-0 z-10 overflow-hidden">
