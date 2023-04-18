1aaf3c2760d268154e4e96ee3db43df426638b5c
Jack wu <jackwu833016@gmail.com>
2023-04-18 10:35:49 -0400

    Remove incorrect migration

--- supabase/migrations/20230408030930_remote_commit.sql
+++ supabase/migrations/20230408030930_remote_commit.sql
@@ -1,28 +1,3 @@
-create type "auth"."code_challenge_method" as enum ('s256', 'plain');
-
-create table "auth"."flow_state" (
-    "id" uuid not null,
-    "user_id" uuid,
-    "auth_code" text not null,
-    "code_challenge_method" auth.code_challenge_method not null,
-    "code_challenge" text not null,
-    "provider_type" text not null,
-    "provider_access_token" text,
-    "provider_refresh_token" text,
-    "created_at" timestamp with time zone,
-    "updated_at" timestamp with time zone
-);
-
-
-CREATE UNIQUE INDEX flow_state_pkey ON auth.flow_state USING btree (id);
-
-CREATE INDEX idx_auth_code ON auth.flow_state USING btree (auth_code);
-
-alter table "auth"."flow_state" add constraint "flow_state_pkey" PRIMARY KEY using index "flow_state_pkey";
-
-
-alter table "storage"."objects" add column "version" text;
-
 set check_function_bodies = off;
 
 CREATE OR REPLACE FUNCTION storage.can_insert_object(bucketid text, name text, owner uuid, metadata jsonb)
