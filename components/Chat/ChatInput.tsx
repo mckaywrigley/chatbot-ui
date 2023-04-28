d5f72884a5e7111cebed42021bc4177a5dc423d8
Tat Dat Duong <david@duong.cz>
2023-04-28 16:38:06 +0200

    Fix styles on mobile

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -312,6 +312,10 @@ export const ChatInput = ({
             </div>
           )}
 
+          <div className="absolute bottom-full md:mb-4 mb-12 mx-auto flex w-full justify-center md:justify-end">
+            <ChatInputTokenCount content={content} />
+          </div>
+
           <textarea
             ref={textareaRef}
             className="m-0 w-full resize-none border-0 bg-transparent p-0 py-2 pr-8 pl-10 text-black dark:bg-transparent dark:text-white md:py-3 md:pl-10"
@@ -336,19 +340,16 @@ export const ChatInput = ({
             onKeyDown={handleKeyDown}
           />
 
-          <div className="absolute right-2 top-2 flex gap-2 items-center">
-            <ChatInputTokenCount content={content} />
-            <button
-              className="rounded-sm p-1 text-neutral-800 opacity-60 hover:bg-neutral-200 hover:text-neutral-900 dark:bg-opacity-50 dark:text-neutral-100 dark:hover:text-neutral-200"
-              onClick={handleSend}
-            >
-              {messageIsStreaming ? (
-                <div className="h-4 w-4 animate-spin rounded-full border-t-2 border-neutral-800 opacity-60 dark:border-neutral-100"></div>
-              ) : (
-                <IconSend size={18} />
-              )}
-            </button>
-          </div>
+          <button
+            className="absolute right-2 top-2 rounded-sm p-1 text-neutral-800 opacity-60 hover:bg-neutral-200 hover:text-neutral-900 dark:bg-opacity-50 dark:text-neutral-100 dark:hover:text-neutral-200"
+            onClick={handleSend}
+          >
+            {messageIsStreaming ? (
+              <div className="h-4 w-4 animate-spin rounded-full border-t-2 border-neutral-800 opacity-60 dark:border-neutral-100"></div>
+            ) : (
+              <IconSend size={18} />
+            )}
+          </button>
 
           {showScrollDownButton && (
             <div className="absolute bottom-12 right-0 lg:bottom-0 lg:-right-10">
