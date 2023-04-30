0d8f8eb7277a45c8f02a034c6d647845b104679d
Jorge Menjivar <jorgemenjivar1997@gmail.com>
2023-04-29 22:22:40 -0700

    Update env variables

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -44,7 +44,7 @@ export const RDBMS_USER = process.env.RDBMS_USER || 'postgres';
 export const RDBMS_PASS = process.env.RDBMS_PASS || 'password';
 
 export const RDBMS_SYNCHRONIZE =
-  process.env.RDBMS_SYNCHRONIZE == 'true' || false;
+  !(process.env.RDBMS_SYNCHRONIZE == 'false') || true;
 
 export const RDBMS_SSL_ENABLED =
   process.env.RDBMS_SSL_ENABLED == 'true' || false;
