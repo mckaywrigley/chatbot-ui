ce590a193075586d0d637f7a148517fd6ef75386
DmitryDenisovCareem <dmitry.denisov@careem.com>
2023-05-06 15:49:42 +0400

    increase bot button

--- components/Chat/ChatMessage.tsx
+++ components/Chat/ChatMessage.tsx
@@ -136,7 +136,7 @@ export const ChatMessage: FC<Props> = memo(({ message, messageIndex, onEdit }) =
       <div className="relative m-auto flex p-4 text-base md:max-w-2xl md:gap-6 md:py-6 lg:max-w-2xl lg:px-0 xl:max-w-3xl">
         <div className="min-w-[40px] text-right font-bold">
           {message.role === 'assistant' ? (
-            <img src="/assets/robot_pic.png" alt="Assistant" width="30" height="30" /> // Use img element with path to the image
+            <img src="/assets/robot_pic.png" alt="Assistant" width="45" height="45" /> // Use img element with path to the image
           ) : (
             <IconUser size={30} />
           )}
