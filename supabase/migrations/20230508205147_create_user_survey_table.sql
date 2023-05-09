cbd1d9f01d37a05fbf15dfe26f1756e4ebd25c76
Jack wu <jackwu833016@gmail.com>
2023-05-09 17:40:47 -0400

    Update migartion to enable insert access but block read access

--- supabase/migrations/20230508205147_create_user_survey_table.sql
+++ supabase/migrations/20230508205147_create_user_survey_table.sql
@@ -1,42 +1,29 @@
 create table "public"."user_survey" (
     "id" uuid not null,
+    "uid" uuid not null references auth.users on delete cascade,
+    "non_login_uid" uuid,
     "created_at" timestamp with time zone default now(),
     "name" text,
     "occupation" text,
     "use_case" jsonb,
     "value_features" jsonb,
     "preferred_choice" jsonb,
-    "comments" text
+    "comments" text,
+    unique ("non_login_uid", "uid")
 );
 
-
+-- Enable row level security
 alter table "public"."user_survey" enable row level security;
 
+-- Create a policy to allow everyone to insert into this table
+create policy "Enable insert for everyone" 
+on "public"."user_survey"
+for insert
+with check (true);
+
+-- Drop the specified columns from the profiles table
 alter table "public"."profiles" drop column "name";
-
 alter table "public"."profiles" drop column "occupation";
-
 alter table "public"."profiles" drop column "preferred_choice";
-
 alter table "public"."profiles" drop column "use_case";
-
 alter table "public"."profiles" drop column "value_features";
-
-CREATE UNIQUE INDEX user_survey_pkey ON public.user_survey USING btree (id);
-
-alter table "public"."user_survey" add constraint "user_survey_pkey" PRIMARY KEY using index "user_survey_pkey";
-
-alter table "public"."user_survey" add constraint "user_survey_id_fkey" FOREIGN KEY (id) REFERENCES profiles(id) ON DELETE CASCADE not valid;
-
-alter table "public"."user_survey" validate constraint "user_survey_id_fkey";
-
-
-CREATE POLICY "Enable insert for authenticated users only" ON "public"."user_survey"
-AS PERMISSIVE FOR INSERT
-TO authenticated
-WITH CHECK (true);
-
-CREATE POLICY "Enable read access for all users" ON "public"."user_survey"
-AS PERMISSIVE FOR SELECT
-TO public
-USING (true);
