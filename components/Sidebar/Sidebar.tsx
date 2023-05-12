228cfc65970c017b4ffd401a0417b3fea42e479e
1orzero <laokameng.6@gmail.com>
2023-05-12 12:31:34 +0800

    Remove comments

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -63,7 +63,6 @@ const Sidebar = <T,>({
 
   return isOpen ? (
     <div>
-      {/* <div>{JSON.stringify(state.isImportingData)}</div> */}
       <div
         className={`fixed top-0 ${side}-0 z-40 flex h-full w-[260px] flex-none flex-col space-y-2 bg-[#202123] p-2 text-[14px] transition-all sm:relative sm:top-0`}
       >
