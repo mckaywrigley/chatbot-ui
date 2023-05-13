6db6301c0b79f8f83848ec5dcf38425adf61dfda
RQ <mengmengmengqiang@gmail.com>
2023-05-14 05:33:14 +0800

    feat: add docker build file

--- script/dockerbuild.sh
+++ script/dockerbuild.sh
@@ -1,9 +1,28 @@
 #!/bin/sh
 
-cd ../
-DTAG=$(date +"%Y%m%d%H%M%S")
-docker build --platform linux/amd64 -f chatbot-ui/Dockerfile -t chatbot-ui .
-docker tag chatbot-ui registry.digitalocean.com/francisco/chatbot-ui:$DTAG
-docker push registry.digitalocean.com/francisco/chatbot-ui:$DTAG
+set -e
 
-echo "registry.digitalocean.com/francisco/chatbot-ui:$DTAG image push success"
+DTAG=$(date +"%Y%m%d%H%M%S")
+echo "Building chatbot-ui image..."
+if docker build --platform linux/amd64 -f ./Dockerfile -t chatbot-ui .; then
+  echo "Image built successfully."
+else
+  echo "Image build failed."
+  exit 1
+fi
+
+echo "Tagging chatbot-ui image..."
+if docker tag chatbot-ui registry.digitalocean.com/francisco/chatbot-ui:$DTAG; then
+  echo "Image tagged successfully."
+else
+  echo "Image tagging failed."
+  exit 1
+fi
+
+echo "Pushing chatbot-ui image..."
+if docker push registry.digitalocean.com/francisco/chatbot-ui:$DTAG; then
+  echo "registry.digitalocean.com/francisco/chatbot-ui:$DTAG image push success"
+else
+  echo "Image push failed."
+  exit 1
+fi
