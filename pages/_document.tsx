bb20c1d842b4abf6b600c4398733993fcf43169c
Rushi Patel <mail.rushipatel@gmail.com>
2023-05-13 18:34:09 -0400

    ad verification tag for Propeller

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,6 +19,8 @@ export default function Document(props: Props) {
         <meta property="og:image" content="https://mugshotbot.com/m/hxYqn5DH" />
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="og:url" content={`https://chateverywhere.app`} />
+        {/* Ad Verification tag for Propellerads*/}
+        <meta name="monetag" content="b0996adc439888b4590068e1e29a5696"/>
       </Head>
       <body>
         <Main />
