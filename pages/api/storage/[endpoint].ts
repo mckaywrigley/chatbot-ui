b953fbf9b25a5a22d0e1a420683edeec7b6833b0
Jorge Menjivar <jorgemenjivar1997@gmail.com>
2023-05-12 23:23:33 -0700

    Remove accidental changes by merge

--- pages/api/storage/[endpoint].ts
+++ pages/api/storage/[endpoint].ts
@@ -1,54 +1,54 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+// import type { NextApiRequest, NextApiResponse } from 'next';
 
-import { AUTH_ENABLED } from 'chatbot-ui-core/utils/const';
+// import { AUTH_ENABLED } from 'chatbot-ui-core/utils/const';
 
-import { User } from 'chatbot-ui-core/types/auth';
+// import { User } from 'chatbot-ui-core/types/auth';
 
-import { getServerSession } from 'chatbot-ui-authjs/server-session';
-import { ServerDatabase } from 'chatbot-ui-core';
-import { ServerSideDatabase } from 'chatbot-ui-rdbms/server-side';
+// import { getServerSession } from 'chatbot-ui-authjs/server-session';
+// import { ServerDatabase } from 'chatbot-ui-core';
+// import { ServerSideDatabase } from 'chatbot-ui-rdbms/server-side';
 
-export default async function proxy(req: NextApiRequest, res: NextApiResponse) {
-  const database: ServerDatabase = new ServerSideDatabase();
-  try {
-    const { endpoint } = req.query;
-    if (!endpoint) {
-      res.status(404).json({ error: 'Endpoint not found' });
-      return;
-    }
-    const path = database.paths.find((path) => path.endpoint === endpoint);
-    if (!path) {
-      res.status(404).json({ error: 'Endpoint not found' });
-      return;
-    }
-    let user: User | null = null;
-    if (AUTH_ENABLED) {
-      const session = await getServerSession(req, res, null);
-      if (!session) {
-        return res.status(401).json({ error: 'User is not authenticated' });
-      } else if (!session.user) {
-        return res.status(401).json({ error: 'User is not authenticated' });
-      } else if (!session.user.email) {
-        return res
-          .status(401)
-          .json({ error: 'User does not have an email address' });
-      }
-      user = {
-        email: session.user.email,
-        image: session.user.image,
-        name: session.user.name,
-      };
-    } else {
-      user = {
-        email: 'default_user',
-        image: null,
-        name: 'Default User',
-      };
-    }
-    // Forward the request and response objects to the handler
-    return path.handler(req, res, user);
-  } catch (error) {
-    console.error('Error in proxy:', error);
-    res.status(500).json({ error: 'Server error' });
-  }
-}
+// export default async function proxy(req: NextApiRequest, res: NextApiResponse) {
+//   const database: ServerDatabase = new ServerSideDatabase();
+//   try {
+//     const { endpoint } = req.query;
+//     if (!endpoint) {
+//       res.status(404).json({ error: 'Endpoint not found' });
+//       return;
+//     }
+//     const path = database.paths.find((path) => path.endpoint === endpoint);
+//     if (!path) {
+//       res.status(404).json({ error: 'Endpoint not found' });
+//       return;
+//     }
+//     let user: User | null = null;
+//     if (AUTH_ENABLED) {
+//       const session = await getServerSession(req, res, null);
+//       if (!session) {
+//         return res.status(401).json({ error: 'User is not authenticated' });
+//       } else if (!session.user) {
+//         return res.status(401).json({ error: 'User is not authenticated' });
+//       } else if (!session.user.email) {
+//         return res
+//           .status(401)
+//           .json({ error: 'User does not have an email address' });
+//       }
+//       user = {
+//         email: session.user.email,
+//         image: session.user.image,
+//         name: session.user.name,
+//       };
+//     } else {
+//       user = {
+//         email: 'default_user',
+//         image: null,
+//         name: 'Default User',
+//       };
+//     }
+//     // Forward the request and response objects to the handler
+//     return path.handler(req, res, user);
+//   } catch (error) {
+//     console.error('Error in proxy:', error);
+//     res.status(500).json({ error: 'Server error' });
+//   }
+// }
