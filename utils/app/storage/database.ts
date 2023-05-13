193d059d0542c6eced6ebea87813ad608f8f74d2
Jorge Menjivar <jorgemenjivar1997@gmail.com>
2023-05-12 18:10:30 -0700

    Create prod version different from main branch

--- utils/app/storage/database.ts
+++ utils/app/storage/database.ts
@@ -1,5 +1,5 @@
 import { Database } from 'chatbot-ui-core';
-import { LocalDatabase } from 'chatbot-ui-local-storage';
+import { ClientSideDatabase } from 'chatbot-ui-rdbms/client-side';
 
 let database: Database | null = null;
 
@@ -7,7 +7,7 @@ export const getDatabase = async () => {
   if (database) {
     return database;
   } else {
-    database = new LocalDatabase();
+    database = new ClientSideDatabase();
     await database.connect();
     return database;
   }
