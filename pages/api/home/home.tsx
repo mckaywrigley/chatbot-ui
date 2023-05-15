8c7688dca0f06794fac380ee3b857ba1291407df
RQ <mengmengmengqiang@gmail.com>
2023-05-15 23:31:22 +0800

    feat: change site icon

--- pages/api/home/home.tsx
+++ pages/api/home/home.tsx
@@ -366,7 +366,7 @@ const Home = ({
           name="viewport"
           content="height=device-height ,width=device-width, initial-scale=1, user-scalable=no"
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/sakura.png" />
       </Head>
       {selectedConversation && (
         <main
