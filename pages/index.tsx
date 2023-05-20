eed61227cc4c53c5c9bd1411784d2bd9584d3d26
DmitryDenisovCareem <dmitry.denisov@careem.com>
2023-05-20 09:45:57 +0400

    add login page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,24 @@
-export { default, getServerSideProps } from './api/home';
+// export { default, getServerSideProps } from './api/home';
+import { useState } from 'react';
+import Link from 'next/link';
+import LoginPage from './LoginPage';
+
+function HomePage() {
+  const [isLoggedIn, setIsLoggedIn] = useState(true); // Change the initial value to true
+
+  // Check if the user is logged in
+  if (isLoggedIn) {
+    // Render the chat with bot component
+    return <div>Chat with bot</div>;
+  } else {
+    // Render the login page
+    return <LoginPage />;
+  }
+}
+
+// Inside the component's render function or JSX block
+<Link href="/login">
+  <a>Login</a>
+</Link>
+
+export default HomePage;
