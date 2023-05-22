a5c266afd077af3da04640aa98451416a0c07192
hamburger <HanJBde@163.com>
2023-05-22 14:03:08 +0800

    log

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -170,10 +170,11 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
             const chunkValue = decoder.decode(value);
             // 解析 chunkValue 为 JSON 对象
             const json = JSON.parse(chunkValue);
+            console.log('json:', json);
             setLastServerMessageId(json.id);
             text += json.text;
             if (isFirst) {
-              console.log('first message:', chunkValue);
+              console.log('first message:', text);
               isFirst = false;
               const updatedMessages: Message[] = [
                 ...updatedConversation.messages,
