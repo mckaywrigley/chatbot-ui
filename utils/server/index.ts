2d4990717235d464074cc3de987f6083730d9450
hamburger <HanJBde@163.com>
2023-05-22 14:42:00 +0800

    log

--- utils/server/index.ts
+++ utils/server/index.ts
@@ -84,7 +84,7 @@ export const OpenAIStream = async (
     }
   }
 
-  const stream = new ReadableStream({
+  return new ReadableStream({
     async start(controller) {
       const onParse = (event: ParsedEvent | ReconnectInterval) => {
         console.log("event:" + JSON.stringify(event));
@@ -116,6 +116,4 @@ export const OpenAIStream = async (
       }
     },
   });
-
-  return stream;
 };
