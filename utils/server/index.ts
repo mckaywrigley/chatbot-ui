a47366d425036c2e06ecda7ba195523724bb8e42
hamburger <HanJBde@163.com>
2023-05-23 12:55:04 +0800

    add express

--- utils/server/index.ts
+++ utils/server/index.ts
@@ -1,127 +1,152 @@
-import { Message } from '@/types/chat';
-import { OpenAIModel } from '@/types/openai';
-
-import { AZURE_DEPLOYMENT_ID, OPENAI_API_HOST, OPENAI_API_TYPE, OPENAI_API_VERSION, OPENAI_ORGANIZATION } from '../app/const';
+import {Message} from '@/types/chat';
+import {OpenAIModel} from '@/types/openai';
+import express from 'express';
 
 import {
-  ParsedEvent,
-  ReconnectInterval,
-  createParser,
+    AZURE_DEPLOYMENT_ID,
+    OPENAI_API_HOST,
+    OPENAI_API_TYPE,
+    OPENAI_API_VERSION,
+    OPENAI_ORGANIZATION
+} from '../app/const';
+
+import {
+    ParsedEvent,
+    ReconnectInterval,
+    createParser,
 } from 'eventsource-parser';
 
 export class OpenAIError extends Error {
-  type: string;
-  param: string;
-  code: string;
+    type: string;
+    param: string;
+    code: string;
 
-  constructor(message: string, type: string, param: string, code: string) {
-    super(message);
-    this.name = 'OpenAIError';
-    this.type = type;
-    this.param = param;
-    this.code = code;
-  }
+    constructor(message: string, type: string, param: string, code: string) {
+        super(message);
+        this.name = 'OpenAIError';
+        this.type = type;
+        this.param = param;
+        this.code = code;
+    }
 }
+
+const app = express();
+
+app.get('/', (req, res) => {
+    let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    if (typeof ip === 'string') {
+        if (ip.substr(0, 7) == "::ffff:") {
+            ip = ip.substr(7) // remove IPv6 prefix
+        }
+    } else if (Array.isArray(ip)) {
+        // if 'x-forwarded-for' header contains multiple IPs, use the first one
+        ip = ip[0];
+    }
+    console.log("real ip:" + ip);
+});
+
+app.listen(3000, () => console.log('Server is running on port 3000'));
+
 
 let sharedVar = "";
 
 function getSharedVar() {
-  return sharedVar;
+    return sharedVar;
 }
 
-export { getSharedVar };
+export {getSharedVar};
 
 export const OpenAIStream = async (
-  model: OpenAIModel,
-  systemPrompt: string,
-  temperature : number,
-  key: string,
-  messages: Message[],
+    model: OpenAIModel,
+    systemPrompt: string,
+    temperature: number,
+    key: string,
+    messages: Message[],
 ) => {
-  let url = `${OPENAI_API_HOST}/v1/chat/completions`;
-  if (OPENAI_API_TYPE === 'azure') {
-    url = `${OPENAI_API_HOST}/openai/deployments/${AZURE_DEPLOYMENT_ID}/chat/completions?api-version=${OPENAI_API_VERSION}`;
-  }
-  const res = await fetch(url, {
-    headers: {
-      'Content-Type': 'application/json',
-      ...(OPENAI_API_TYPE === 'openai' && {
-        Authorization: `Bearer ${key ? key : process.env.OPENAI_API_KEY}`
-      }),
-      ...(OPENAI_API_TYPE === 'azure' && {
-        'api-key': `${key ? key : process.env.OPENAI_API_KEY}`
-      }),
-      ...((OPENAI_API_TYPE === 'openai' && OPENAI_ORGANIZATION) && {
-        'OpenAI-Organization': OPENAI_ORGANIZATION,
-      }),
-    },
-    method: 'POST',
-    body: JSON.stringify({
-      ...(OPENAI_API_TYPE === 'openai' && {model: model.id}),
-      messages: [
-        {
-          role: 'system',
-          content: systemPrompt,
-        },
-        ...messages,
-      ],
-      max_tokens: 1000,
-      temperature: temperature,
-      stream: true,
-    }),
-  });
-
-  const encoder = new TextEncoder();
-  const decoder = new TextDecoder();
-  if (res.status !== 200) {
-    const result = await res.json();
-    if (result.error) {
-      throw new OpenAIError(
-        result.error.message,
-        result.error.type,
-        result.error.param,
-        result.error.code,
-      );
-    } else {
-      throw new Error(
-        `OpenAI API returned an error: ${
-          decoder.decode(result?.value) || result.statusText
-        }`,
-      );
+    let url = `${OPENAI_API_HOST}/v1/chat/completions`;
+    if (OPENAI_API_TYPE === 'azure') {
+        url = `${OPENAI_API_HOST}/openai/deployments/${AZURE_DEPLOYMENT_ID}/chat/completions?api-version=${OPENAI_API_VERSION}`;
     }
-  }
+    const res = await fetch(url, {
+        headers: {
+            'Content-Type': 'application/json',
+            ...(OPENAI_API_TYPE === 'openai' && {
+                Authorization: `Bearer ${key ? key : process.env.OPENAI_API_KEY}`
+            }),
+            ...(OPENAI_API_TYPE === 'azure' && {
+                'api-key': `${key ? key : process.env.OPENAI_API_KEY}`
+            }),
+            ...((OPENAI_API_TYPE === 'openai' && OPENAI_ORGANIZATION) && {
+                'OpenAI-Organization': OPENAI_ORGANIZATION,
+            }),
+        },
+        method: 'POST',
+        body: JSON.stringify({
+            ...(OPENAI_API_TYPE === 'openai' && {model: model.id}),
+            messages: [
+                {
+                    role: 'system',
+                    content: systemPrompt,
+                },
+                ...messages,
+            ],
+            max_tokens: 1000,
+            temperature: temperature,
+            stream: true,
+        }),
+    });
 
-  return new ReadableStream({
-    async start(controller) {
-      const onParse = (event: ParsedEvent | ReconnectInterval) => {
-        if (event.type === 'event') {
-          const data = event.data;
-
-          try {
-            const json = JSON.parse(data);
-            if (json.choices[0].finish_reason != null) {
-              controller.close();
-              return;
-            }
-            sharedVar = json.id
-            const text = json.choices[0].delta.content;
-            if (!text) {
-              controller.enqueue(encoder.encode("id:[" + json.id + "]"));
-            } else {
-              const queue = encoder.encode(text);
-              controller.enqueue(queue);
-            }
-          } catch (e) {
-            controller.error(e);
-          }
+    const encoder = new TextEncoder();
+    const decoder = new TextDecoder();
+    if (res.status !== 200) {
+        const result = await res.json();
+        if (result.error) {
+            throw new OpenAIError(
+                result.error.message,
+                result.error.type,
+                result.error.param,
+                result.error.code,
+            );
+        } else {
+            throw new Error(
+                `OpenAI API returned an error: ${
+                    decoder.decode(result?.value) || result.statusText
+                }`,
+            );
         }
-      };
+    }
 
-      const parser = createParser(onParse);
+    return new ReadableStream({
+        async start(controller) {
+            const onParse = (event: ParsedEvent | ReconnectInterval) => {
+                if (event.type === 'event') {
+                    const data = event.data;
 
-      for await (const chunk of res.body as any) {
-        parser.feed(decoder.decode(chunk));
-      }
-    },
-  });
+                    try {
+                        const json = JSON.parse(data);
+                        if (json.choices[0].finish_reason != null) {
+                            controller.close();
+                            return;
+                        }
+                        sharedVar = json.id
+                        const text = json.choices[0].delta.content;
+                        if (!text) {
+                            controller.enqueue(encoder.encode("id:[" + json.id + "]"));
+                        } else {
+                            const queue = encoder.encode(text);
+                            controller.enqueue(queue);
+                        }
+                    } catch (e) {
+                        controller.error(e);
+                    }
+                }
+            };
+
+            const parser = createParser(onParse);
+
+            for await (const chunk of res.body as any) {
+                parser.feed(decoder.decode(chunk));
+            }
+        },
+    });
 };
