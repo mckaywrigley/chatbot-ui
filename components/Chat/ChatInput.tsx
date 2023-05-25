ba9c81f52b24b02c5f274084ce5a33dca032c2f8
tyrannic <337266440@qq.com>
2023-05-25 10:05:39 +0800

    test pipeline

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -380,17 +380,10 @@ export const ChatInput = ({
         </div>
       </div>
       <div className="px-3 pt-2 pb-3 text-center text-[12px] text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6">
-        <a
-          href="https://github.com/mckaywrigley/chatbot-ui"
-          target="_blank"
-          rel="noreferrer"
-          className="underline"
-        >
-          ChatBot UI
-        </a>
+      
         .{' '}
         {t(
-          "Chatbot UI is an advanced chatbot kit for OpenAI's chat models aiming to mimic ChatGPT's interface and functionality.",
+          "Congratulations, Jun Zhun and Jiayu Tao, on your engagement! May your love and commitment to each other bring you endless joy and happiness. ",
         )}
       </div>
     </div>
