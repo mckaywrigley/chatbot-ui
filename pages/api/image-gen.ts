5517317b08aea2f11b65357ca07736d2fc254080
Jack wu <jackwu833016@gmail.com>
2023-05-27 16:32:07 -0400

    Initial implementation of MidJourney img generation

--- pages/api/image-gen.ts
+++ pages/api/image-gen.ts
@@ -24,52 +24,11 @@ export const config = {
   runtime: 'edge',
 };
 
+const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
+
 const unauthorizedResponse = new Response('Unauthorized', { status: 401 });
 
-// Reference https://platform.stability.ai/rest-api#tag/v1generation/operation/textToImage
-const IMAGE_GENERATION_CONFIG = {
-  engineName: 'stable-diffusion-xl-beta-v2-2-2',
-  height: 512,
-  width: 512,
-  cfgScale: 4,
-  steps: 50,
-  negativePrompts: 'blur, low resolution',
-};
-
-const CREATIVELY_LEVELS_MAPPING = {
-  high: {
-    steps: 40,
-    cfgScale: 2,
-  },
-  medium: {
-    steps: 50,
-    cfgScale: 4,
-  },
-  low: {
-    steps: 100,
-    cfgScale: 7,
-  },
-};
-
-// The lower the temperature, the less creative the generation
-const getImageGenerationConfig = (temperature: number = 0.5) => {
-  if (temperature < 0.3) {
-    return {
-      ...IMAGE_GENERATION_CONFIG,
-      ...CREATIVELY_LEVELS_MAPPING.low,
-    };
-  } else if (temperature < 0.7) {
-    return {
-      ...IMAGE_GENERATION_CONFIG,
-      ...CREATIVELY_LEVELS_MAPPING.medium,
-    };
-  } else {
-    return {
-      ...IMAGE_GENERATION_CONFIG,
-      ...CREATIVELY_LEVELS_MAPPING.high,
-    };
-  }
-};
+const MAX_TIMEOUT = 300; // 5 minutes
 
 const handler = async (req: Request): Promise<Response> => {
   const userToken = req.headers.get('user-token');
@@ -95,103 +54,124 @@ const handler = async (req: Request): Promise<Response> => {
     await writer.write(encoder.encode(text));
   };
 
-  try {
-    const requestBody = (await req.json()) as ChatBody;
+  const requestBody = (await req.json()) as ChatBody;
 
-    const imageGeneration = async () => {
+  const imageGeneration = async () => {
+    const requestHeader = {
+      Authorization: `Bearer ${process.env.THE_NEXT_LEG_API_KEY || ''}`,
+      'Content-Type': 'application/json',
+    };
+
+    try {
       const latestUserPromptMessage =
         requestBody.messages[requestBody.messages.length - 1].content;
 
-      const generationTemperature = requestBody.temperature;
-      const numberOfImages =
-        requestBody.numberOfSamples || DEFAULT_IMAGE_GENERATION_SAMPLE;
-      const imageStyle =
-        requestBody.imageStyle || DEFAULT_IMAGE_GENERATION_STYLE;
-
       writeToStream('```Image Generation \n');
-      writeToStream('Generating image(s) ... \n');
-
-      const imageGenerationConfig = getImageGenerationConfig(
-        generationTemperature,
-      );
+      writeToStream('Generating ... \n');
 
       const options = {
         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${process.env.STABILITY_AI_KEY}`,
-        },
+        headers: requestHeader,
         body: JSON.stringify({
-          text_prompts: [
-            {
-              text: latestUserPromptMessage,
-              weight: 0.5,
-            },
-            {
-              text: imageGenerationConfig.negativePrompts,
-              weight: -0.5,
-            },
-          ],
-          height: imageGenerationConfig.height,
-          width: imageGenerationConfig.width,
-          style_preset: imageStyle,
-          cfg_scale: imageGenerationConfig.cfgScale,
-          steps: imageGenerationConfig.steps,
+          msg: latestUserPromptMessage,
         }),
       };
 
-      for (let imageIndex = 0; imageIndex < numberOfImages; imageIndex++) {
-        const imageGenerationResponse = await fetch(
-          `https://api.stability.ai/v1/generation/${IMAGE_GENERATION_CONFIG.engineName}/text-to-image`,
-          options,
+      const imageGenerationResponse = await fetch(
+        `https://api.thenextleg.io/v2/imagine`,
+        options,
+      );
+
+      if (!imageGenerationResponse.ok) {
+        throw new Error('Image generation failed');
+      }
+
+      const imageGenerationResponseJson = await imageGenerationResponse.json();
+
+      if (
+        imageGenerationResponseJson.success !== true ||
+        !imageGenerationResponseJson.messageId
+      ) {
+        console.log(imageGenerationResponseJson);
+        console.error('Failed during submitting request');
+        throw new Error('Image generation failed');
+      }
+
+      const imageGenerationMessageId = imageGenerationResponseJson.messageId;
+
+      // Check every 4 seconds if the image generation is done
+      let generationStartedAt = Date.now();
+      let imageGenerationProgress = null;
+
+      while (
+        Date.now() - generationStartedAt < MAX_TIMEOUT * 1000 ||
+        imageGenerationProgress < 100
+      ) {
+        await sleep(4000);
+        const imageGenerationProgressResponse = await fetch(
+          `https://api.thenextleg.io/v2/message/${imageGenerationMessageId}?authToken=${process.env.THE_NEXT_LEG_API_KEY}`,
+          { method: 'GET' },
         );
 
-        const imageGenerationResponseJson =
-          await imageGenerationResponse.json();
-        const base64ImageString =
-          imageGenerationResponseJson.artifacts[0].base64;
-
-        // Upload base64 Image to Supabase Storage
-        const imageFileName = `${user.id}-${v4()}.png`;
-        const { error } = await supabase.storage
-          .from('ai-images')
-          .upload(imageFileName, decode(base64ImageString), {
-            cacheControl: '3600',
-            upsert: false,
-          });
-        if (error) throw error;
-
-        const { data: imagePublicUrlData } = await supabase.storage
-          .from('ai-images')
-          .getPublicUrl(imageFileName);
-
-        if (imageIndex === 0) {
-          writeToStream(`Receiving image(s) ... \n`);
-          writeToStream('``` \n');
+        if (!imageGenerationProgressResponse.ok) {
+          console.log(await imageGenerationProgressResponse.status);
+          console.log(await imageGenerationProgressResponse.text());
+          throw new Error('Unable to fetch image generation progress');
         }
 
-        writeToStream(`![Generated Image](${imagePublicUrlData.publicUrl}) \n`);
+        const imageGenerationProgressResponseJson =
+          await imageGenerationProgressResponse.json();
 
-        addUsageEntry(PluginID.IMAGE_GEN, user.id);
-        subtractCredit(user.id, PluginID.IMAGE_GEN);
+        console.log(imageGenerationProgressResponseJson);
+
+        const generationProgress = imageGenerationProgressResponseJson.progress;
+
+        if (generationProgress === 100) {
+          writeToStream(`100% ... \n`);
+          writeToStream('``` \n');
+
+          writeToStream(
+            `![Generated Image](${imageGenerationProgressResponseJson.response.imageUrl}) \n`,
+          );
+          addUsageEntry(PluginID.IMAGE_GEN, user.id);
+          subtractCredit(user.id, PluginID.IMAGE_GEN);
+          imageGenerationProgress = 100;
+
+          await writeToStream('[DONE]');
+          writer.close();
+          return;
+        } else if (generationProgress !== imageGenerationProgress) {
+          writeToStream(`${generationProgress}% ... \n`);
+          imageGenerationProgress = generationProgress;
+        }
       }
 
       await writeToStream('[DONE]');
+      await writeToStream(
+        'Unable to finish the generation in 5 minutes, please try again later.',
+      );
       writer.close();
-    };
+      return;
+    } catch (error) {
+      console.log(error);
 
-    imageGeneration();
+      await writeToStream(
+        'Error occurred while generating image, please try again later.',
+      );
+      await writeToStream('[DONE]');
+      writer.close();
+      return;
+    }
+  };
 
-    return new NextResponse(stream.readable, {
-      headers: {
-        'Content-Type': 'text/event-stream',
-        'Cache-Control': 'no-cache',
-      },
-    });
-  } catch (error) {
-    writer.close();
-    return new Response('Error', { status: 500 });
-  }
+  imageGeneration();
+
+  return new NextResponse(stream.readable, {
+    headers: {
+      'Content-Type': 'text/event-stream',
+      'Cache-Control': 'no-cache',
+    },
+  });
 };
 
 export default handler;
