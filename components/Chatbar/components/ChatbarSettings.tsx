1a5de3882fc030390c2f68f3bab135e5e23d3c5e
1orzero <laokameng.6@gmail.com>
2023-05-31 09:11:21 +0800

    Chatbar setting add min height

--- components/Chatbar/components/ChatbarSettings.tsx
+++ components/Chatbar/components/ChatbarSettings.tsx
@@ -74,7 +74,7 @@ export const ChatbarSettings = () => {
   };
 
   return (
-    <>
+    <div className="min-h-min">
       <CloudSyncStatusComponent />
       <div className="flex flex-col items-center space-y-1 border-t border-white/20 pt-1 text-sm overflow-auto">
         {conversations.length > 0 ? (
@@ -144,6 +144,6 @@ export const ChatbarSettings = () => {
           }}
         />
       </div>
-    </>
+    </div>
   );
 };
