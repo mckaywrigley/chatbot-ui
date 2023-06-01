c4a9fd8269b30c6f7d22c7c9ed86d0d128b69775
hamburger <HanJBde@163.com>
2023-06-01 19:44:20 +0800

    blog v4

--- pages/BlogPage.js
+++ pages/BlogPage.js
@@ -147,7 +147,7 @@ function BlogPage() {
               .content {
                 flex: 1 1 auto;
                 overflow-y: auto;
-                background-color: #fff;
+                background-color: #EEEEEE;
               }
             `}</style>
         </div>
