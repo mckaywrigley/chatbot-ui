024dc6719285f5ce14f5c0f3860e87d5a8c21064
chang <changlee99@gmail.com>
2023-06-11 15:31:44 +0900

    aa

--- utils/app/clean.ts
+++ utils/app/clean.ts
@@ -16,7 +16,7 @@ export const cleanSelectedConversation = (conversation: Conversation) => {
   if (!updatedConversation.model) {
     updatedConversation = {
       ...updatedConversation,
-      model: updatedConversation.model || OpenAIModels[OpenAIModelID.GPT_3_5],
+      model: updatedConversation.model || OpenAIModels[OpenAIModelID.GPT_CHANG],
     };
   }
 
@@ -67,7 +67,7 @@ export const cleanConversationHistory = (history: any[]): Conversation[] => {
   return history.reduce((acc: any[], conversation) => {
     try {
       if (!conversation.model) {
-        conversation.model = OpenAIModels[OpenAIModelID.GPT_3_5];
+        conversation.model = OpenAIModels[OpenAIModelID.GPT_CHANG];
       }
 
       if (!conversation.prompt) {
