c7ba11f40885c50938bb8d42d7082f663f124099
teramotodaiki <i101323@gmail.com>
2023-06-17 19:15:02 +0900

    モデルの整理

--- types/openai.ts
+++ types/openai.ts
@@ -8,13 +8,11 @@ export interface OpenAIModel {
 }
 
 export enum OpenAIModelID {
-  GPT_3_5 = 'gpt-3.5-turbo',
-  GPT_3_5_0613 = 'gpt-3.5-turbo-0613',
+  GPT_3_5 = 'gpt-3.5-turbo-0613',
   GPT_3_5_16K = 'gpt-3.5-turbo-16k',
   GPT_3_5_AZ = 'gpt-35-turbo',
-  GPT_4 = 'gpt-4',
-  GPT_4_32K = 'gpt-4-32k',
-  GPT_4_0613 = 'gpt-4-0613',
+  GPT_4 = 'gpt-4-0613',
+  // GPT_4_32K = 'gpt-4-32k',
 }
 
 // in case the `DEFAULT_MODEL` environment variable is not set or set to an unsupported model
@@ -33,33 +31,15 @@ export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
     maxLength: 12000,
     tokenLimit: 4000,
   },
-  [OpenAIModelID.GPT_3_5_0613]: {
-    id: OpenAIModelID.GPT_3_5_0613,
-    name: 'GPT-3.5',
-    maxLength: 12000,
-    tokenLimit: 4000,
-  },
   [OpenAIModelID.GPT_3_5_16K]: {
     id: OpenAIModelID.GPT_3_5_16K,
-    name: 'GPT-3.5',
+    name: 'GPT-3.5-16K',
     maxLength: 12000,
     tokenLimit: 4000,
   },
   [OpenAIModelID.GPT_4]: {
     id: OpenAIModelID.GPT_4,
     name: 'GPT-4',
-    maxLength: 24000,
-    tokenLimit: 8000,
-  },
-  [OpenAIModelID.GPT_4_32K]: {
-    id: OpenAIModelID.GPT_4_32K,
-    name: 'GPT-4-32K',
-    maxLength: 96000,
-    tokenLimit: 32000,
-  },
-  [OpenAIModelID.GPT_4_0613]: {
-    id: OpenAIModelID.GPT_4_0613,
-    name: 'GPT-4-0613',
     maxLength: 24000,
     tokenLimit: 8000,
   },
