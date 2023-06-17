a1b2a178712a945a663a7d0ff09d8c7b2a4012d7
teramotodaiki <i101323@gmail.com>
2023-06-17 19:05:26 +0900

    モデル追加

--- types/openai.ts
+++ types/openai.ts
@@ -9,6 +9,8 @@ export interface OpenAIModel {
 
 export enum OpenAIModelID {
   GPT_3_5 = 'gpt-3.5-turbo',
+  GPT_3_5_0613 = 'gpt-3.5-turbo-0613',
+  GPT_3_5_16K = 'gpt-3.5-turbo-16k',
   GPT_3_5_AZ = 'gpt-35-turbo',
   GPT_4 = 'gpt-4',
   GPT_4_32K = 'gpt-4-32k',
@@ -27,6 +29,18 @@ export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
   },
   [OpenAIModelID.GPT_3_5_AZ]: {
     id: OpenAIModelID.GPT_3_5_AZ,
+    name: 'GPT-3.5',
+    maxLength: 12000,
+    tokenLimit: 4000,
+  },
+  [OpenAIModelID.GPT_3_5_0613]: {
+    id: OpenAIModelID.GPT_3_5_0613,
+    name: 'GPT-3.5',
+    maxLength: 12000,
+    tokenLimit: 4000,
+  },
+  [OpenAIModelID.GPT_3_5_16K]: {
+    id: OpenAIModelID.GPT_3_5_16K,
     name: 'GPT-3.5',
     maxLength: 12000,
     tokenLimit: 4000,
