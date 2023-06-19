f7cad6f32a4542016536c5dcc62b641373635761
Nils Jacobsen <58360188+NilsJacobsen@users.noreply.github.com>
2023-06-19 12:52:02 +0000

    fix: update config

--- inlang.config.js
+++ inlang.config.js
@@ -3,7 +3,7 @@ export async function defineConfig(env) {
 		"https://cdn.jsdelivr.net/npm/@inlang/plugin-i18next@2/dist/index.js",
 	)
   const { default: standardLintRules } = await env.$import(
-		"https://cdn.jsdelivr.net/gh/inlang/standard-lint-rules@2/dist/index.js",
+		"https://cdn.jsdelivr.net/npm/@inlang/plugin-standard-lint-rules@3/dist/index.js",
 	)
 
 	return {
