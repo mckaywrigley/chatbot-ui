6ea621bea6559f8df5560d0bb0e8406d35e61d6e
blazst <blahos@wp.pl>
2023-06-27 11:09:56 +0200

    feat(chat):add workspace

--- components/Workspace/Workspace.tsx
+++ components/Workspace/Workspace.tsx
@@ -1,40 +1,88 @@
 import { ChangeEvent, FC, useState } from 'react';
 
+import {
+  API_ENTRYPOINT,
+  PRIVATE_API_ENTRYPOINT,
+  WORKSPACES_ENDPOINT,
+} from '@/utils/app/const';
+
+interface Workspace {
+  name: string;
+  slug: string;
+  logo: string;
+}
+console.log('OUTSIDE COMP');
+console.log(API_ENTRYPOINT);
+
+const workspaces: Workspace[] = [
+  { name: 'Legal', slug: 'legal', logo: 'url-to-legal.png' },
+  {
+    name: 'Risk & Compliance',
+    slug: 'risk-compliance',
+    logo: 'url-to-risk.png',
+  },
+];
+
 export const Workspace: FC = () => {
-  const [selectValue, setSelectValue] = useState('');
-  const workspaceChangeHandler = (event: ChangeEvent<HTMLSelectElement>) => {
-    if (event.target.value === 'Workspace1') {
-      setSelectValue('Workspace1');
-      console.log('Workspace1 selected');
-      // async () => {
-      //   const response = await fetch('...workspace1');
-      //   const data = await response.json();
-      //   console.log(data);
-      // };
-    }
-    if (event.target.value === 'Workspace2') {
-      setSelectValue('Workspace2');
-      console.log('Workspace2 selected');
-      // async () => {
-      //   const response = await fetch('...workspace2');
-      //   const data = await response.json();
-      //   console.log(data);
-      // };
+  // const PRIVATE_API_ENTRYPOINT = `${API_ENTRYPOINT}/private`;
+  // const WORKSPACES_ENDPOINT = `${PRIVATE_API_ENTRYPOINT}/workspaces`;
+  console.log('***********');
+  console.log(API_ENTRYPOINT);
+  console.log(WORKSPACES_ENDPOINT);
+
+  const [selectedValue, setSelectedValue] = useState(workspaces[0].slug);
+  const [selectedWorkspace, setSelectedWorkspace] = useState<Workspace>(
+    workspaces[0],
+  );
+
+  const selectWorkspaceHandler = (workspace: Workspace) => {
+    setSelectedWorkspace(workspace);
+  };
+
+  const APICall = async () => {
+    const response = await fetch(WORKSPACES_ENDPOINT, {
+      method: 'POST',
+      body: JSON.stringify(selectedWorkspace!.slug),
+    });
+    const data = await response.json();
+    console.log(data);
+    //do sth with the post response data
+  };
+
+  APICall();
+  // api call to have default workspace without choosing it in select bar
+  const workspaceAPICallHandler = (event: ChangeEvent<HTMLSelectElement>) => {
+    setSelectedValue(event.target.value);
+    //event.target.value === workspace.slug
+    try {
+      APICall();
+    } catch (error) {
+      console.log(error);
     }
   };
+
   return (
-    <div className="m-2 rounded border border-neutral-200 bg-transparent pr-2  text-neutral-900 dark:border-neutral-600 dark:text-white hover:bg-gray-500/10  tranistion-colors duration-200">
+    <div className="m-2 rounded border border-neutral-200 bg-transparent pr-2  light:text-neutral-900 dark:border-neutral-600 dark:text-white hover:bg-gray-500/10  tranistion-colors duration-200">
       <select
         className="w-full cursor-pointer bg-transparent p-2"
-        onChange={workspaceChangeHandler}
-        value={selectValue}
+        onChange={workspaceAPICallHandler}
+        value={selectedValue}
       >
-        <option className="bg-[#202123]" value="Workspace1">
-          Workspace1
+        <option disabled={true} className="bg-[#202123]">
+          Select workspace
         </option>
-        <option className="bg-[#202123]" value="Workspace2">
-          Workspace2
-        </option>
+        {workspaces.map((workspace) => (
+          <option
+            className="bg-[#202123]"
+            value={workspace.slug}
+            key={workspace.slug}
+            onClick={() => {
+              selectWorkspaceHandler(workspace);
+            }}
+          >
+            {workspace.name}
+          </option>
+        ))}
       </select>
     </div>
   );
--- utils/app/const.ts
+++ utils/app/const.ts
@@ -5,17 +5,24 @@ export const DEFAULT_SYSTEM_PROMPT =
 export const OPENAI_API_HOST =
   process.env.OPENAI_API_HOST || 'https://api.openai.com';
 
-export const DEFAULT_TEMPERATURE = 
-  parseFloat(process.env.NEXT_PUBLIC_DEFAULT_TEMPERATURE || "1");
+export const DEFAULT_TEMPERATURE = parseFloat(
+  process.env.NEXT_PUBLIC_DEFAULT_TEMPERATURE || '1',
+);
 
-export const OPENAI_API_TYPE =
-  process.env.OPENAI_API_TYPE || 'openai';
+export const OPENAI_API_TYPE = process.env.OPENAI_API_TYPE || 'openai';
 
 export const OPENAI_API_VERSION =
   process.env.OPENAI_API_VERSION || '2023-03-15-preview';
 
-export const OPENAI_ORGANIZATION =
-  process.env.OPENAI_ORGANIZATION || '';
+export const OPENAI_ORGANIZATION = process.env.OPENAI_ORGANIZATION || '';
 
-export const AZURE_DEPLOYMENT_ID =
-  process.env.AZURE_DEPLOYMENT_ID || '';
+export const AZURE_DEPLOYMENT_ID = process.env.AZURE_DEPLOYMENT_ID || '';
+
+export const API_ENTRYPOINT =
+  process.env.API_ENTRYPOINT || 'SumUp Azure OpenAI Chat UI';
+
+export const PRIVATE_API_ENTRYPOINT =
+  process.env.PRIVATE_API_ENTRYPOINT || 'SumUp Azure OpenAI Chat UI';
+
+export const WORKSPACES_ENDPOINT =
+  process.env.WORKSPACES_ENDPOINT || 'SumUp Azure OpenAI Chat UI';
