31933e397a6bccdf138103419eb5e985bceffdab
sabinbaniya <dummymail@tutanota.de>
2023-07-02 15:31:42 +0545

    feat: fix default screen text about selecting api

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -359,30 +359,20 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
           </div>
           <div className="text-center text-gray-500 dark:text-gray-400">
             <div className="mb-2">
-              Chatbot UI allows you to plug in your BitAPAI API key to use this
-              UI with their API.
+              Chatbot UI allows you to plug in your API key to use this UI with
+              their API.
             </div>
             <div className="mb-2">
               It is <span className="italic">only</span> used to communicate
               with their API.
             </div>
             <div className="mb-2">
-              {t(
-                'Please set your Bitapai API key in the bottom left of the sidebar.',
-              )}
+              {t('Please set your API key in the bottom left of the sidebar.')}
             </div>
             <div>
               {t(
-                "If you don't have an BitAPAI API key, you can get one here: ",
+                "If you don't have an API key, please get one form respective website.",
               )}
-              <a
-                href="https://bitapai.io/"
-                target="_blank"
-                rel="noreferrer"
-                className="text-blue-500 hover:underline"
-              >
-                bitapai.io
-              </a>
             </div>
           </div>
         </div>
