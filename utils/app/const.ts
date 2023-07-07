97b176a2d9e6d09298649831ae63152fec2b4a0c
warjiang <1096409085@qq.com>
2023-07-07 17:49:24 +0800

    feat: add support for basepath

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -2,6 +2,8 @@ export const DEFAULT_SYSTEM_PROMPT =
   process.env.NEXT_PUBLIC_DEFAULT_SYSTEM_PROMPT ||
   "You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown.";
 
+export const BASEPATH = process.env.BASEPATH || process.env.NEXT_PUBLIC_BASEPATH || '';
+
 export const OPENAI_API_HOST =
   process.env.OPENAI_API_HOST || 'https://api.openai.com';
 
