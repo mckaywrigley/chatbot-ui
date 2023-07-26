08a76d91689f0a6687c5d7263f5aa75be83dbafd
William Tan <1284324+Ninja3047@users.noreply.github.com>
2023-07-26 16:06:41 -0400

    add missing model name

--- types/openai.ts
+++ types/openai.ts
@@ -12,6 +12,7 @@ export enum OpenAIModelID {
   GPT_3_5_AZ = 'gpt-35-turbo',
   GPT_4 = 'gpt-4',
   GPT_4_32K = 'gpt-4-32k',
+  GPT_35_TURBO_16K = "gpt-35-turbo-16k",
 }
 
 // in case the `DEFAULT_MODEL` environment variable is not set or set to an unsupported model
@@ -41,5 +42,11 @@ export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
     name: 'GPT-4-32K',
     maxLength: 96000,
     tokenLimit: 32000,
+  },
+  [OpenAIModelID.GPT_35_TURBO_16K]: {
+    id: OpenAIModelID.GPT_35_TURBO_16K,
+    name: "GPT 3.5 Turbo",
+    maxLength: 12000,
+    tokenLimit: 4000,
   },
 };
