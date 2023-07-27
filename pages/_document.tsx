147a45ec9b4caad5678cd2ccec7daee9f1572f13
lsylsy186 <tommy450845821@gmail.com>
2023-07-27 23:02:40 +0800

    feat: 移除ga，改为va

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,5 @@
 import { DocumentProps, Head, Html, Main, NextScript } from 'next/document';
-import Script from 'next/script'
+// import Script from 'next/script'
 
 import i18nextConfig from '../next-i18next.config';
 
@@ -16,7 +16,7 @@ export default function Document(props: Props) {
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-title" content="Chatbot UI"></meta>
         {/* <!-- Google tag (gtag.js) --> */}
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-W5KW6VWGBH"></Script>
+        {/* <Script async src="https://www.googletagmanager.com/gtag/js?id=G-W5KW6VWGBH"></Script>
         <Script id="google-analytics">
           {
             `
@@ -26,25 +26,24 @@ export default function Document(props: Props) {
             
               gtag('config', 'G-W5KW6VWGBH');`
           }
-        </Script>
+        </Script> */}
         {/* <!-- Google Tag Manager --> */}
-        <Script id="google-tag-manager">
+        {/* <Script id="google-tag-manager">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-NXP873G6');
-          `}</Script>
-        {/* <!-- End Google Tag Manager --> */}
+          `}</Script> */}
       </Head>
       <body>
         {/* <!-- Google Tag Manager (noscript) --> */}
-        <noscript>
+        {/* <noscript>
           {`
             <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NXP873G6"
             height="0" width="0" style="display:none;visibility:hidden"></iframe>
           `}
-        </noscript>
+        </noscript> */}
         {/* <!-- End Google Tag Manager (noscript) --> */}
         <Main />
         <NextScript />
