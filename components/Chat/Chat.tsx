a897427cac28c3d569266d49189ea19d90f7fa57
Marcin Wozniak <marcin.wozniak@theengineers.tech>
2023-08-09 15:20:53 +0200

    fix(TH3-165): fixed bug of google search plugin (#46)

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -250,7 +250,7 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
             };
             homeDispatch({
               field: 'selectedConversation',
-              value: updateConversation,
+              value: updatedConversation,
             });
             saveConversation(updatedConversation);
             const updatedConversations: Conversation[] = conversations.map(
