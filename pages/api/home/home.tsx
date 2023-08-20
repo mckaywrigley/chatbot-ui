05e7c33583df6bb49e8537afa86da04862afe706
rosbel <rlsanroman@gmail.com>
2023-08-20 13:18:02 -0500

    Update title

--- pages/api/home/home.tsx
+++ pages/api/home/home.tsx
@@ -360,7 +360,7 @@ const Home = ({
       }}
     >
       <Head>
-        <title>Chatbot UI</title>
+        <title>chat.rosbel.ai - Chatbot UI</title>
         <meta name="description" content="ChatGPT but better." />
         <meta
           name="viewport"
