1209afa6a5ccf858ddc5f595c22d66209e145a7a
Srujan Gurram <srujangs8@gmail.com>
2023-09-06 20:52:34 +0530

    Refactor GenerateParametersGenerateParameters in schema.ts to use deepPartial

--- utils/server/schema.ts
+++ utils/server/schema.ts
@@ -1,23 +1,25 @@
 import { z } from 'zod';
 
-const GenerateParametersGenerateParameters = z.object({
-  best_of: z.union([z.literal(null), z.number().min(0)]).nullable(),
-  decoder_input_details: z.boolean().default(true),
-  details: z.boolean().default(true),
-  do_sample: z.boolean().default(false),
-  max_new_tokens: z.number().int().min(0).max(511),
-  repetition_penalty: z.union([z.literal(null), z.number().min(0)]).nullable(),
-  return_full_text: z.union([z.literal(null), z.boolean()]).nullable(),
-  seed: z.union([z.literal(null), z.number().int().min(0)]).nullable(),
-  stop: z.array(z.string()).max(4),
-  temperature: z.union([z.literal(null), z.number().min(0)]).nullable(),
-  top_k: z.union([z.literal(null), z.number().int().min(0)]).nullable(),
-  top_n_tokens: z.union([z.literal(null), z.number().int().min(0)]).nullable(),
-  top_p: z.union([z.literal(null), z.number().min(0).max(1)]).nullable(),
-  truncate: z.union([z.literal(null), z.number().int().min(0)]).nullable(),
-  typical_p: z.union([z.literal(null), z.number().min(0).max(1)]).nullable(),
-  watermark: z.boolean().default(false),
-});
+const GenerateParametersGenerateParameters = z
+  .object({
+    best_of: z.number().min(0),
+    decoder_input_details: z.boolean().default(true),
+    details: z.boolean().default(true),
+    do_sample: z.boolean().default(false),
+    max_new_tokens: z.number().int().min(0).max(511),
+    repetition_penalty: z.number().min(0),
+    return_full_text: z.boolean(),
+    seed: z.number().int().min(0),
+    stop: z.array(z.string()).max(4),
+    temperature: z.number().min(0),
+    top_k: z.number().int().min(0),
+    top_n_tokens: z.number().int().min(0),
+    top_p: z.number().min(0).max(1),
+    truncate: z.number().int().min(0),
+    typical_p: z.number().min(0).max(1),
+    watermark: z.boolean().default(false),
+  })
+  .deepPartial();
 
 const InputSchema = z.object({
   inputs: z.string(),
