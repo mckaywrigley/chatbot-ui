e2313cff81cf14755b14148c2af0b50e14ba8d67
Srujan Gurram <srujangs8@gmail.com>
2023-09-07 09:14:26 +0530

    Refactor OpenCloseButton component styles to improve readability and consistency

--- components/Sidebar/components/OpenCloseButton.tsx
+++ components/Sidebar/components/OpenCloseButton.tsx
@@ -11,7 +11,7 @@ export const CloseSidebarButton = ({ onClick, side }: Props) => {
       <button
         className={`fixed top-5 ${
           side === 'right' ? 'right-[270px]' : 'left-[270px]'
-        } z-50 h-7 w-7 hover:text-gray-400 dark:text-white dark:hover:text-gray-300 sm:top-0.5 sm:${
+        } z-50 h-7 w-7 hover:text-gray-400 dark:text-gray-500 dark:hover:text-gray-300 sm:top-0.5 sm:${
           side === 'right' ? 'right-[270px]' : 'left-[270px]'
         } sm:h-8 sm:w-8 sm:text-gray-700`}
         onClick={onClick}
@@ -31,7 +31,7 @@ export const OpenSidebarButton = ({ onClick, side }: Props) => {
     <button
       className={`fixed top-2.5 ${
         side === 'right' ? 'right-2' : 'left-2'
-      } z-50 h-7 w-7 text-white hover:text-gray-400 dark:text-white dark:hover:text-gray-300 sm:top-0.5 sm:${
+      } z-50 h-7 w-7 text-white hover:text-gray-400 dark:text-gray-500 dark:hover:text-gray-300 sm:top-0.5 sm:${
         side === 'right' ? 'right-2' : 'left-2'
       } sm:h-8 sm:w-8 sm:text-gray-700`}
       onClick={onClick}
