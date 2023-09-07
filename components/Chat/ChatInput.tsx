9a8568daab2b6100f4cdd2faa2cea4db6de93c22
Srujan Gurram <srujangs8@gmail.com>
2023-09-07 07:29:27 +0530

    Remove unnecessary imports and update the description of the project

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -1,7 +1,5 @@
 import {
   IconArrowDown,
-  IconBolt,
-  IconBrandGoogle,
   IconPlayerStop,
   IconRepeat,
   IconSend,
@@ -345,17 +343,8 @@ export const ChatInput = ({
         </div>
       </div>
       <div className="px-3 pt-2 pb-3 text-center text-[12px] text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6">
-        <a
-          href="https://github.com/mckaywrigley/chatbot-ui"
-          target="_blank"
-          rel="noreferrer"
-          className="underline"
-        >
-          Sol IQGPT
-        </a>
-        .{' '}
         {t(
-          "Sol IQGPT is an advanced chatbot kit for OpenAI's chat models aiming to mimic ChatGPT's interface and functionality.",
+          'Sol IQGPT is an a project from IQ.Wiki to generate smart contract code from natural language.',
         )}
       </div>
     </div>
