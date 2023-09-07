5108f1d31f4670f8df9105752e48df23eec150f0
Srujan Gurram <srujangs8@gmail.com>
2023-09-07 08:55:28 +0530

    Update background color and hover effect for Conversation, Folder, and Prompt components in Chatbar, Folder, and Promptbar respectively

--- components/Chatbar/components/Conversation.tsx
+++ components/Chatbar/components/Conversation.tsx
@@ -103,7 +103,7 @@ export const ConversationComponent = ({ conversation }: Props) => {
   return (
     <div className="relative flex items-center">
       {isRenaming && selectedConversation?.id === conversation.id ? (
-        <div className="flex w-full items-center gap-3 rounded-lg bg-[#343541]/90 p-3">
+        <div className="flex w-full items-center gap-3 rounded-lg bg-black/10 p-3">
           <IconMessage size={18} />
           <input
             className="mr-12 flex-1 overflow-hidden overflow-ellipsis border-gray-400 bg-transparent text-left text-[12.5px] leading-3 text-white outline-none focus:border-gray-100"
@@ -116,12 +116,10 @@ export const ConversationComponent = ({ conversation }: Props) => {
         </div>
       ) : (
         <button
-          className={`flex w-full cursor-pointer items-center gap-3 rounded-lg p-3 text-sm transition-colors duration-200 hover:bg-[#343541]/90 ${
+          className={`flex w-full cursor-pointer items-center gap-3 rounded-lg p-3 text-sm transition-colors duration-200 hover:bg-black/10 ${
             messageIsStreaming ? 'disabled:cursor-not-allowed' : ''
           } ${
-            selectedConversation?.id === conversation.id
-              ? 'bg-[#343541]/90'
-              : ''
+            selectedConversation?.id === conversation.id ? 'bg-black/10' : ''
           }`}
           onClick={() => handleSelectConversation(conversation)}
           disabled={messageIsStreaming}
