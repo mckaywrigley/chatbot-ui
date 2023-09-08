4df53f5e32458e782a17b770cd90c5828e3e20ac
yuta.koga <yuta.koga@leverages.jp>
2023-09-08 11:22:26 +0900

    正しくシステムプロンプトを表示

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -1,5 +1,6 @@
 export const DEFAULT_SYSTEM_PROMPT =
   process.env.NEXT_PUBLIC_DEFAULT_SYSTEM_PROMPT ||
+  // "You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown.";
   "You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully.";
 
 export const OPENAI_API_HOST =
