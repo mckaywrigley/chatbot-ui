164fff3c36ba74dac3b8b582881afde73aff3905
Srujan Gurram <srujangs8@gmail.com>
2023-09-08 10:52:59 +0530

    Update the CSS styling for the folder name container to limit its width to 65% of the parent element

--- components/Folder/Folder.tsx
+++ components/Folder/Folder.tsx
@@ -125,7 +125,7 @@ const Folder = ({
               <IconCaretRight size={18} />
             )}
 
-            <div className="relative max-h-5 flex-1 overflow-hidden text-ellipsis whitespace-nowrap break-all text-left text-[12.5px] leading-3">
+            <div className="relative max-h-5 max-w-[65%] flex-1 overflow-hidden text-ellipsis whitespace-nowrap break-all text-left text-[12.5px] leading-3">
               {currentFolder.name}
             </div>
           </button>
