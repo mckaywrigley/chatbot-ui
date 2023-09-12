bf3ee9b96ce4a6e701df5c0071f2dc0505e8693f
jldroid <10508346+jldroid19@users.noreply.github.com>
2023-09-12 10:48:22 -0400

    Update build-local.sh

--- build-local.sh
+++ build-local.sh
@@ -3,6 +3,7 @@
 # curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 # sudo apt-get install -y nodejs
 
+nvm install 20
 
 # Pull the latest changes from your Git repository (if you are using version control)
 # Uncomment the next line if you are using Git
@@ -18,7 +19,6 @@ npm run build
 echo "running docker compose"
 
 docker-compose --env-file .env.local up
-
 
 
 #npm run start
