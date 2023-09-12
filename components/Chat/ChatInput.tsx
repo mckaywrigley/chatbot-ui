65125a80b36ba77d44dea61daaf01e68be02df08
b-solution <bilel.kedidi@gmail.com>
2023-09-12 12:31:13 +0500

    Tweaks

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -492,7 +492,6 @@ export const ChatInput = ({
             </div>
             <div
                 className="px-3 pt-2 pb-3 text-center text-[12px] text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6">
-                up {showScrollUpButton ? 'true' : 'false'} down {showScrollDownButton ? 'true' : 'false'}
                 <a
                     href="https://github.com/mckaywrigley/chatbot-ui"
                     target="_blank"
