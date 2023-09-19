459a826711770bdd462df006e9981ef2d3ec7cbe
b-solution <bilel.kedidi@gmail.com>
2023-09-19 15:19:44 +0500

    Fixed vertical scrollbar issue to include all content in Settings div

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -430,8 +430,8 @@ export const Chat = memo(({stopConversationRef}: Props) => {
                     >
                         {selectedConversation?.messages?.length === 0 ? (
                             <>
-                                <div
-                                    className="mx-auto flex flex-col space-y-5 md:space-y-10 px-3 pt-5 md:pt-12 sm:max-w-[600px]">
+                                <div className="h-[calc(100vh-162px)]">
+                                    <div className="mx-auto flex flex-col space-y-5 md:space-y-10 px-3 pt-5 md:pt-12 sm:max-w-[600px]">
                                     <div
                                         className="text-center text-3xl font-semibold text-gray-800 dark:text-gray-100">
                                         {models.length === 0 ? (
@@ -470,6 +470,7 @@ export const Chat = memo(({stopConversationRef}: Props) => {
                                             />
                                         </div>
                                     )}
+                                </div>
                                 </div>
                             </>
                         ) : (
