9e4e70270f1aae9f0f04731b686efed28cd529f1
Charles C. Lee <charleschanlee@gmail.com>
2023-12-12 12:01:09 -0800

    Convert to using the pure JS port of tiktoken

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -2,13 +2,13 @@ import { DEFAULT_SYSTEM_PROMPT, DEFAULT_TEMPERATURE } from '@/utils/app/const';
 import { OpenAIError, OpenAIStream } from '@/utils/server';
 
 import { ChatBody, Message } from '@/types/chat';
-
-// @ts-expect-error
-import wasm from '../../node_modules/@dqbd/tiktoken/lite/tiktoken_bg.wasm?module';
-
-import tiktokenModel from '@dqbd/tiktoken/encoders/cl100k_base.json';
-import { Tiktoken, init } from '@dqbd/tiktoken/lite/init';
 import { OpenAIModelID, OpenAIModels } from '@/types/openai';
+
+import {
+  TiktokenModel,
+  getEncoding,
+  getEncodingNameForModel,
+} from 'js-tiktoken';
 
 export const config = {
   runtime: 'edge',
@@ -19,7 +19,7 @@ const handler = async (req: Request): Promise<Response> => {
 
   if (typeof req !== 'undefined') {
     const traceHeader = req.headers.get('X-Cloud-Trace-Context');
-      if (traceHeader && Boolean(process.env.GCP_PROJECT)) {
+    if (traceHeader && Boolean(process.env.GCP_PROJECT)) {
       const [trace] = traceHeader.split('/');
       globalLogFields[
         'logging.googleapis.com/trace'
@@ -28,16 +28,14 @@ const handler = async (req: Request): Promise<Response> => {
   }
 
   try {
-    const { model, messages, key, prompt, temperature } = (await req.json()) as ChatBody;
+    const { model, messages, key, prompt, temperature } =
+      (await req.json()) as ChatBody;
 
-    const {tokenLimit, messageTokens} = OpenAIModels[model.id as OpenAIModelID];
+    const { tokenLimit, messageTokens } =
+      OpenAIModels[model.id as OpenAIModelID];
 
-    await init((imports) => WebAssembly.instantiate(wasm, imports));
-    const encoding = new Tiktoken(
-      tiktokenModel.bpe_ranks,
-      tiktokenModel.special_tokens,
-      tiktokenModel.pat_str,
-    );
+    const encodingForModel = getEncodingNameForModel(model.id as TiktokenModel);
+    const encoding = getEncoding(encodingForModel);
 
     let promptToSend = prompt;
     if (!promptToSend) {
@@ -63,50 +61,66 @@ const handler = async (req: Request): Promise<Response> => {
     for (let i = messages.length - 1; i >= 0; i--) {
       const message = messages[i];
 
-      const msgTokenCount = messageTokens +
+      const msgTokenCount =
+        messageTokens +
         encoding.encode(message.content).length +
         encoding.encode(message.role).length;
 
-      if (tokenCount + msgTokenCount + maxReplyTokens + safetyMargin > tokenLimit) {
+      if (
+        tokenCount + msgTokenCount + maxReplyTokens + safetyMargin >
+        tokenLimit
+      ) {
         break;
       }
       tokenCount += msgTokenCount;
       messagesToSend = [message, ...messagesToSend];
     }
 
-    encoding.free();
-
-    const stream = await OpenAIStream(model, promptToSend, temperatureToUse, key, messagesToSend);
+    const stream = await OpenAIStream(
+      model,
+      promptToSend,
+      temperatureToUse,
+      key,
+      messagesToSend,
+    );
 
     return new Response(stream);
   } catch (error) {
     let msg = null;
 
     if (error instanceof OpenAIError) {
-      console.error(JSON.stringify(Object.assign(
-        {
-          severity: 'ERROR',
-          message: error.message,
-          name: error.name,
-          type: error.type,
-          param: error.param,
-          code: error.code,
-        },
-        globalLogFields
-      )));
+      console.error(
+        JSON.stringify(
+          Object.assign(
+            {
+              severity: 'ERROR',
+              message: error.message,
+              name: error.name,
+              type: error.type,
+              param: error.param,
+              code: error.code,
+            },
+            globalLogFields,
+          ),
+        ),
+      );
       msg = error.message;
     } else {
-      console.error(JSON.stringify(Object.assign(
-        { severity: 'ERROR', message: String(error), },
-        globalLogFields
-      )));
-      msg = "Internal Server Error";
+      console.error(
+        JSON.stringify(
+          Object.assign(
+            { severity: 'ERROR', message: String(error) },
+            globalLogFields,
+          ),
+        ),
+      );
+      msg = 'Internal Server Error';
     }
 
-    return new Response(
-      JSON.stringify({ error: msg }),
-      { status: 500, headers: { 'Content-Type': 'application/json' }},
-    );
+    return new Response(JSON.stringify({ error: msg }), {
+      status: 500,
+      headers: { 'Content-Type': 'application/json' },
+    });
   }
 };
 
