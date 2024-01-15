e2edd4a734990ef3c1800943a229411bff71710a
Mckay Wrigley <mckaywrigley@gmail.com>
2024-01-15 07:52:01 -0700

    add forgot password (#1178)

---
+++ app/login/password/page.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import { ChangePassword } from "@/components/utility/change-password"
+import { supabase } from "@/lib/supabase/browser-client"
+import { useRouter } from "next/navigation"
+import { useEffect, useState } from "react"
+
+export default function ChangePasswordPage() {
+  const [loading, setLoading] = useState(true)
+
+  const router = useRouter()
+
+  useEffect(() => {
+    ;(async () => {
+      const session = (await supabase.auth.getSession()).data.session
+
+      if (!session) {
+        router.push("/login")
+      } else {
+        setLoading(false)
+      }
+    })()
+  }, [])
+
+  if (loading) {
+    return null
+  }
+
+  return <ChangePassword />
+}
