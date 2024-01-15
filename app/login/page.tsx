7a0afb0f6ccf825d1cfbde046380099560f68558
Mckay Wrigley <mckaywrigley@gmail.com>
2024-01-15 07:51:29 -0700

    add forgot password

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -54,7 +54,6 @@ export default async function Login({
   const signUp = async (formData: FormData) => {
     "use server"
 
-    const origin = headers().get("origin")
     const email = formData.get("email") as string
     const password = formData.get("password") as string
     const cookieStore = cookies()
@@ -80,6 +79,24 @@ export default async function Login({
     // return redirect("/login?message=Check email to continue sign in process")
   }
 
+  const handleResetPassword = async (formData: FormData) => {
+    "use server"
+
+    const origin = headers().get("origin")
+    const email = formData.get("email") as string
+    const cookieStore = cookies()
+    const supabase = createClient(cookieStore)
+
+    const { error } = await supabase.auth.resetPasswordForEmail(email, {
+      redirectTo: `${origin}/auth/callback?next=/login/password`
+    })
+
+    if (error) {
+      console.error(error)
+      return redirect("/login?message=Could not reset password")
+    }
+  }
+
   return (
     <div className="flex w-full flex-1 flex-col justify-center gap-2 px-8 sm:max-w-md">
       <form
@@ -92,7 +109,7 @@ export default async function Login({
           Email
         </Label>
         <Input
-          className="mb-6 rounded-md border bg-inherit px-4 py-2"
+          className="mb-3 rounded-md border bg-inherit px-4 py-2"
           name="email"
           placeholder="you@example.com"
           required
@@ -106,7 +123,6 @@ export default async function Login({
           type="password"
           name="password"
           placeholder="••••••••"
-          required
         />
 
         <Button className="mb-2 rounded-md bg-blue-700 px-4 py-2 text-white">
@@ -119,6 +135,16 @@ export default async function Login({
         >
           Sign Up
         </Button>
+
+        <div className="text-muted-foreground mt-1 flex justify-center text-sm">
+          <span className="mr-1">Forgot your password?</span>
+          <button
+            formAction={handleResetPassword}
+            className="text-primary ml-1 underline hover:opacity-80"
+          >
+            Reset
+          </button>
+        </div>
 
         {searchParams?.message && (
           <p className="bg-foreground/10 text-foreground mt-4 p-4 text-center">
