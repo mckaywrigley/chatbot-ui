d3cf8b23bcc7af550540ac08104418ecc00bc57e
Mckay Wrigley <mckaywrigley@gmail.com>
2024-01-18 17:49:49 -0700

    remove old code

--- app/page.tsx
+++ app/page.tsx
@@ -4,38 +4,9 @@ import { ChatbotUISVG } from "@/components/icons/chatbotui-svg"
 import { IconArrowRight } from "@tabler/icons-react"
 import { useTheme } from "next-themes"
 import Link from "next/link"
-import { useEffect, useState } from "react"
 
 export default function HomePage() {
   const { theme } = useTheme()
-
-  const [stars, setStars] = useState(19000)
-
-  useEffect(() => {
-    getGitHubRepoStars()
-  }, [])
-
-  const getGitHubRepoStars = async () => {
-    const url = `https://api.github.com/repos/mckaywrigley/chatbot-ui`
-
-    try {
-      const response = await fetch(url, {
-        headers: {
-          Accept: "application/vnd.github.v3+json"
-        }
-      })
-
-      if (!response.ok) {
-        throw new Error(`Error: ${response.status}`)
-      }
-
-      const data = await response.json()
-      setStars(data.stargazers_count)
-    } catch (error) {
-      console.error("Failed to fetch GitHub stars:", error)
-      return 0
-    }
-  }
 
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center">
