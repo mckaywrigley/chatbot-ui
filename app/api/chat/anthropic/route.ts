c4daf87a0fbcb3b9bef579e649f4331eb330dcfe
Mckay Wrigley <mckaywrigley@gmail.com>
2024-01-24 10:30:25 -0700

    disable anthropic edge runtime due to bug

--- app/api/chat/anthropic/route.ts
+++ app/api/chat/anthropic/route.ts
@@ -4,7 +4,7 @@ import { ChatSettings } from "@/types"
 import Anthropic from "@anthropic-ai/sdk"
 import { AnthropicStream, StreamingTextResponse } from "ai"
 
-export const runtime = "edge"
+// export const runtime = "edge"
 
 export async function POST(request: Request) {
   const json = await request.json()
