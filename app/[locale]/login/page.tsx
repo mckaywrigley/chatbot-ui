e5e83fffa0faae14cf6ee737e237fb4ed7e3d7f8
Kelvin Wong <61614687+im-calvin@users.noreply.github.com>
2024-01-23 16:08:55 -0800

    fix origin not defined

--- app/[locale]/login/page.tsx
+++ app/[locale]/login/page.tsx
@@ -60,6 +60,7 @@ export default async function Login({
   const signUp = async (formData: FormData) => {
     "use server"
 
+    const origin = headers().get("origin")
     const email = formData.get("email") as string
     const password = formData.get("password") as string
 
