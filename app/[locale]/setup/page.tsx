0abc081494e4f2f37614c13735bc8d6258f83533
Mckay Wrigley <mckaywrigley@gmail.com>
2024-01-24 16:07:54 -0700

    simplify setup

--- app/[locale]/setup/page.tsx
+++ app/[locale]/setup/page.tsx
@@ -2,11 +2,9 @@
 
 import { ChatbotUIContext } from "@/context/context"
 import { getProfileByUserId, updateProfile } from "@/db/profile"
-import { uploadImage } from "@/db/storage/profile-images"
-import { getWorkspacesByUserId, updateWorkspace } from "@/db/workspaces"
+import { getWorkspacesByUserId } from "@/db/workspaces"
 import { supabase } from "@/lib/supabase/browser-client"
 import { TablesUpdate } from "@/supabase/types"
-import { ChatSettings } from "@/types"
 import { useRouter } from "next/navigation"
 import { useContext, useEffect, useState } from "react"
 import { APIStep } from "../../../components/setup/api-step"
@@ -16,10 +14,9 @@ import {
   SETUP_STEP_COUNT,
   StepContainer
 } from "../../../components/setup/step-container"
-import { WorkspaceStep } from "../../../components/setup/workspace-step"
 
 export default function SetupPage() {
-  const { profile, setProfile, setSelectedWorkspace, setWorkspaces } =
+  const { profile, setProfile, setWorkspaces, setSelectedWorkspace } =
     useContext(ChatbotUIContext)
 
   const router = useRouter()
@@ -29,12 +26,9 @@ export default function SetupPage() {
   const [currentStep, setCurrentStep] = useState(1)
 
   // Profile Step
-  const [profileContext, setProfileContext] = useState("")
   const [displayName, setDisplayName] = useState("")
   const [username, setUsername] = useState(profile?.username || "")
   const [usernameAvailable, setUsernameAvailable] = useState(true)
-  const [profileImageSrc, setProfileImageSrc] = useState("")
-  const [profileImage, setProfileImage] = useState<File | null>(null)
 
   // API Step
   const [useAzureOpenai, setUseAzureOpenai] = useState(false)
@@ -45,23 +39,34 @@ export default function SetupPage() {
   const [azureOpenai35TurboID, setAzureOpenai35TurboID] = useState("")
   const [azureOpenai45TurboID, setAzureOpenai45TurboID] = useState("")
   const [azureOpenai45VisionID, setAzureOpenai45VisionID] = useState("")
+  const [azureOpenaiEmbeddingsID, setAzureOpenaiEmbeddingsID] = useState("")
   const [anthropicAPIKey, setAnthropicAPIKey] = useState("")
   const [googleGeminiAPIKey, setGoogleGeminiAPIKey] = useState("")
   const [mistralAPIKey, setMistralAPIKey] = useState("")
   const [perplexityAPIKey, setPerplexityAPIKey] = useState("")
   const [openrouterAPIKey, setOpenrouterAPIKey] = useState("")
 
-  // Workspace Step
-  const [workspaceInstructions, setWorkspaceInstructions] = useState("")
-  const [defaultChatSettings, setDefaultChatSettings] = useState<ChatSettings>({
-    model: "gpt-4-1106-preview",
-    prompt: "You are a friendly, helpful AI assistant.",
-    temperature: 0.5,
-    contextLength: 4096,
-    includeProfileContext: true,
-    includeWorkspaceInstructions: true,
-    embeddingsProvider: "openai"
-  })
+  useEffect(() => {
+    ;(async () => {
+      const session = (await supabase.auth.getSession()).data.session
+
+      if (!session) {
+        router.push("/login")
+      } else {
+        const user = session.user
+
+        const profile = await getProfileByUserId(user.id)
+        setProfile(profile)
+        setUsername(profile.username)
+
+        if (!profile.has_onboarded) {
+          setLoading(false)
+        } else {
+          router.push("/chat")
+        }
+      }
+    })()
+  }, [])
 
   const handleShouldProceed = (proceed: boolean) => {
     if (proceed) {
@@ -85,23 +90,11 @@ export default function SetupPage() {
     const user = session.user
     const profile = await getProfileByUserId(user.id)
 
-    let profileImageUrl = ""
-    let profileImagePath = ""
-
-    if (profileImage) {
-      const { path, url } = await uploadImage(profile, profileImage)
-      profileImageUrl = url
-      profileImagePath = path
-    }
-
     const updateProfilePayload: TablesUpdate<"profiles"> = {
       ...profile,
       has_onboarded: true,
       display_name: displayName,
       username,
-      profile_context: profileContext,
-      image_url: profileImageUrl,
-      image_path: profileImageUrl,
       openai_api_key: openaiAPIKey,
       openai_organization_id: openaiOrgID,
       anthropic_api_key: anthropicAPIKey,
@@ -114,40 +107,19 @@ export default function SetupPage() {
       azure_openai_endpoint: azureOpenaiEndpoint,
       azure_openai_35_turbo_id: azureOpenai35TurboID,
       azure_openai_45_turbo_id: azureOpenai45TurboID,
-      azure_openai_45_vision_id: azureOpenai45VisionID
+      azure_openai_45_vision_id: azureOpenai45VisionID,
+      azure_openai_embeddings_id: azureOpenaiEmbeddingsID
     }
 
     const updatedProfile = await updateProfile(profile.id, updateProfilePayload)
-
     setProfile(updatedProfile)
-
-    const updateHomeWorkspacePayload: TablesUpdate<"workspaces"> = {
-      default_context_length: defaultChatSettings.contextLength,
-      default_model: defaultChatSettings.model,
-      default_prompt: defaultChatSettings.prompt,
-      default_temperature: defaultChatSettings.temperature,
-      include_profile_context: defaultChatSettings.includeProfileContext,
-      include_workspace_instructions:
-        defaultChatSettings.includeWorkspaceInstructions,
-      instructions: workspaceInstructions,
-      embeddings_provider: defaultChatSettings.embeddingsProvider
-    }
 
     const workspaces = await getWorkspacesByUserId(profile.user_id)
     const homeWorkspace = workspaces.find(w => w.is_home)
 
     // There will always be a home workspace
-    const updatedWorkspace = await updateWorkspace(
-      homeWorkspace!.id,
-      updateHomeWorkspacePayload
-    )
-
-    setSelectedWorkspace(updatedWorkspace)
-    setWorkspaces(
-      workspaces.map(workspace =>
-        workspace.id === updatedWorkspace.id ? updatedWorkspace : workspace
-      )
-    )
+    setSelectedWorkspace(homeWorkspace!)
+    setWorkspaces(workspaces)
 
     router.refresh()
   }
@@ -166,15 +138,9 @@ export default function SetupPage() {
             showBackButton={false}
           >
             <ProfileStep
-              profileContext={profileContext}
-              profileImageSrc={profileImageSrc}
-              profileImage={profileImage}
               username={username}
               usernameAvailable={usernameAvailable}
               displayName={displayName}
-              onProfileContextChange={setProfileContext}
-              onProfileImageChangeSrc={setProfileImageSrc}
-              onProfileImageChange={setProfileImage}
               onUsernameAvailableChange={setUsernameAvailable}
               onUsernameChange={setUsername}
               onDisplayNameChange={setDisplayName}
@@ -188,7 +154,7 @@ export default function SetupPage() {
           <StepContainer
             stepDescription="Enter API keys for each service you'd like to use."
             stepNum={currentStep}
-            stepTitle="Set API Keys"
+            stepTitle="Set API Keys (optional)"
             onShouldProceed={handleShouldProceed}
             showNextButton={true}
             showBackButton={true}
@@ -201,6 +167,7 @@ export default function SetupPage() {
               azureOpenai35TurboID={azureOpenai35TurboID}
               azureOpenai45TurboID={azureOpenai45TurboID}
               azureOpenai45VisionID={azureOpenai45VisionID}
+              azureOpenaiEmbeddingsID={azureOpenaiEmbeddingsID}
               anthropicAPIKey={anthropicAPIKey}
               googleGeminiAPIKey={googleGeminiAPIKey}
               mistralAPIKey={mistralAPIKey}
@@ -213,6 +180,7 @@ export default function SetupPage() {
               onAzureOpenai35TurboIDChange={setAzureOpenai35TurboID}
               onAzureOpenai45TurboIDChange={setAzureOpenai45TurboID}
               onAzureOpenai45VisionIDChange={setAzureOpenai45VisionID}
+              onAzureOpenaiEmbeddingsIDChange={setAzureOpenaiEmbeddingsID}
               onAnthropicAPIKeyChange={setAnthropicAPIKey}
               onGoogleGeminiAPIKeyChange={setGoogleGeminiAPIKey}
               onMistralAPIKeyChange={setMistralAPIKey}
@@ -224,28 +192,8 @@ export default function SetupPage() {
           </StepContainer>
         )
 
-      // Workspace Step
-      case 3:
-        return (
-          <StepContainer
-            stepDescription="Select the default settings for your home workspace."
-            stepNum={currentStep}
-            stepTitle="Create Workspace"
-            onShouldProceed={handleShouldProceed}
-            showNextButton={true}
-            showBackButton={true}
-          >
-            <WorkspaceStep
-              chatSettings={defaultChatSettings}
-              workspaceInstructions={workspaceInstructions}
-              onChatSettingsChange={setDefaultChatSettings}
-              onWorkspaceInstructionsChange={setWorkspaceInstructions}
-            />
-          </StepContainer>
-        )
-
       // Finish Step
-      case 4:
+      case 3:
         return (
           <StepContainer
             stepDescription="You are all set up!"
@@ -262,28 +210,6 @@ export default function SetupPage() {
         return null
     }
   }
-
-  useEffect(() => {
-    ;(async () => {
-      const session = (await supabase.auth.getSession()).data.session
-
-      if (!session) {
-        router.push("/login")
-      } else {
-        const user = session.user
-
-        const profile = await getProfileByUserId(user.id)
-        setProfile(profile)
-        setUsername(profile.username)
-
-        if (!profile.has_onboarded) {
-          setLoading(false)
-        } else {
-          router.push("/chat")
-        }
-      }
-    })()
-  }, [])
 
   if (loading) {
     return null
--- components/setup/finish-step.tsx
+++ components/setup/finish-step.tsx
@@ -7,7 +7,10 @@ interface FinishStepProps {
 export const FinishStep: FC<FinishStepProps> = ({ displayName }) => {
   return (
     <div className="space-y-4">
-      <div>Welcome to Chatbot UI, {displayName.split(" ")[0]}!</div>
+      <div>
+        Welcome to Chatbot UI
+        {displayName.length > 0 ? `, ${displayName.split(" ")[0]}` : null}!
+      </div>
 
       <div>Click next to start chatting.</div>
     </div>
