8898e92e8eefb1bb558ece8b4c700c25cf9c034c
Mckay Wrigley <mckaywrigley@gmail.com>
2024-01-28 17:55:34 -0700

    prep command keys for @ assistants

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -179,7 +179,7 @@ export const ChatInput: FC<ChatInputProps> = ({}) => {
           textareaRef={chatInputRef}
           className="ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring text-md flex w-full resize-none rounded-md border-none bg-transparent px-14 py-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
           placeholder={t(
-            `Ask anything. Type "/" for prompts, "@" for files, and "#" for tools.`
+            `Ask anything. Type "/" for prompts, "#" for files, and "!" for tools.`
           )}
           onValueChange={handleInputChange}
           value={userInput}
