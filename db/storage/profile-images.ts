10e666b0ed8d43b699e3d47a7d0388e02318b26f
Mckay Wrigley <mckaywrigley@gmail.com>
2024-01-29 13:40:51 -0700

    fix profile image caching issues

--- db/storage/profile-images.ts
+++ db/storage/profile-images.ts
@@ -1,16 +1,30 @@
 import { supabase } from "@/lib/supabase/browser-client"
 import { Tables } from "@/supabase/types"
 
-export const uploadImage = async (profile: Tables<"profiles">, image: File) => {
+export const uploadProfileImage = async (
+  profile: Tables<"profiles">,
+  image: File
+) => {
   const bucket = "profile_images"
 
-  const imageSizeLimit = 6000000 // 6MB
+  const imageSizeLimit = 2000000 // 2MB
 
   if (image.size > imageSizeLimit) {
     throw new Error(`Image must be less than ${imageSizeLimit / 1000000}MB`)
   }
 
-  let filePath = `${profile.user_id}/${profile.id}`
+  const currentPath = profile.image_path
+  let filePath = `${profile.user_id}/${Date.now()}`
+
+  if (currentPath.length > 0) {
+    const { error: deleteError } = await supabase.storage
+      .from(bucket)
+      .remove([currentPath])
+
+    if (deleteError) {
+      throw new Error("Error deleting old image")
+    }
+  }
 
   const { error } = await supabase.storage
     .from(bucket)
