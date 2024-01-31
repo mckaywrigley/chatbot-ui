674772146aa62c960b5095d60cccde6afa16a4e4
Mckay Wrigley <mckaywrigley@gmail.com>
2024-01-30 23:56:48 -0700

    remove package

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -12,10 +12,10 @@ import { ReactNode } from "react"
 import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
-const APP_NAME = "Chatbot UI";
-const APP_DEFAULT_TITLE = "Chatbot UI";
-const APP_TITLE_TEMPLATE = "%s - Chatbot UI";
-const APP_DESCRIPTION = "Chabot UI PWA!";
+const APP_NAME = "Chatbot UI"
+const APP_DEFAULT_TITLE = "Chatbot UI"
+const APP_TITLE_TEMPLATE = "%s - Chatbot UI"
+const APP_DESCRIPTION = "Chabot UI PWA!"
 
 interface RootLayoutProps {
   children: ReactNode
@@ -28,41 +28,41 @@ export const metadata: Metadata = {
   applicationName: APP_NAME,
   title: {
     default: APP_DEFAULT_TITLE,
-    template: APP_TITLE_TEMPLATE,
+    template: APP_TITLE_TEMPLATE
   },
   description: APP_DESCRIPTION,
   manifest: "/manifest.json",
   appleWebApp: {
     capable: true,
     statusBarStyle: "black",
-    title: APP_DEFAULT_TITLE,
+    title: APP_DEFAULT_TITLE
     // startUpImage: [],
   },
   formatDetection: {
-    telephone: false,
+    telephone: false
   },
   openGraph: {
     type: "website",
     siteName: APP_NAME,
     title: {
       default: APP_DEFAULT_TITLE,
-      template: APP_TITLE_TEMPLATE,
+      template: APP_TITLE_TEMPLATE
     },
-    description: APP_DESCRIPTION,
+    description: APP_DESCRIPTION
   },
   twitter: {
     card: "summary",
     title: {
       default: APP_DEFAULT_TITLE,
-      template: APP_TITLE_TEMPLATE,
+      template: APP_TITLE_TEMPLATE
     },
-    description: APP_DESCRIPTION,
-  },
-};
+    description: APP_DESCRIPTION
+  }
+}
 
 export const viewport: Viewport = {
-  themeColor: "#000000",
-};
+  themeColor: "#000000"
+}
 
 const i18nNamespaces = ["translation"]
 
