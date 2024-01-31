a635959cea0123c2a6517c4468be8ff612d6b8a9
davecrab <65996799+davecrab@users.noreply.github.com>
2024-01-30 22:57:32 -0800

    Setup PWA (#1334)
    * Setup PWA components
    
    Added pwa components
    
    * Update package.json
    
    * Update package.json
    
    added missing dependency
    
    * Update layout.tsx
    
    * remove package
    
    ---------
    
    Co-authored-by: Mckay Wrigley <mckaywrigley@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,27 +1,34 @@
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true"
-})
+});
 
-/** @type {import('next').NextConfig} */
-module.exports = withBundleAnalyzer({
-  reactStrictMode: false,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost"
-      },
-      {
-        protocol: "http",
-        hostname: "127.0.0.1"
-      },
-      {
-        protocol: "https",
-        hostname: "**"
-      }
-    ]
-  },
-  experimental: {
-    serverComponentsExternalPackages: ["sharp", "onnxruntime-node"]
-  }
-})
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+});
+
+module.exports = withBundleAnalyzer(
+  withPWA({
+    reactStrictMode: true,
+    images: {
+      remotePatterns: [
+        {
+          protocol: "http",
+          hostname: "localhost"
+        },
+        {
+          protocol: "http",
+          hostname: "127.0.0.1"
+        },
+        {
+          protocol: "https",
+          hostname: "**"
+        }
+      ]
+    },
+    experimental: {
+      serverComponentsExternalPackages: ["sharp", "onnxruntime-node"]
+    }
+  })
+);
