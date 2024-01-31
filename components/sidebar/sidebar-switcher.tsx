8c65dad55c6e1fccc7bd710929bef84a8895d3d2
= <gerosullivan@gmail.com>
2024-01-31 16:02:32 +0000

    Refactor to simplify sidebar and profile settings components

--- components/sidebar/sidebar-switcher.tsx
+++ components/sidebar/sidebar-switcher.tsx
@@ -9,7 +9,8 @@ import {
   IconRobotFace,
   IconSparkles
 } from "@tabler/icons-react"
-import { FC } from "react"
+import { ChatbotUIContext } from "@/context/context"
+import { FC, useContext } from "react"
 import { TabsList } from "../ui/tabs"
 import { WithTooltip } from "../ui/with-tooltip"
 import { ProfileSettings } from "../utility/profile-settings"
@@ -24,6 +25,8 @@ interface SidebarSwitcherProps {
 export const SidebarSwitcher: FC<SidebarSwitcherProps> = ({
   onContentTypeChange
 }) => {
+  const { profile } = useContext(ChatbotUIContext)
+
   return (
     <div className="flex flex-col justify-between border-r-2 pb-5">
       <TabsList className="bg-background grid h-[440px] grid-rows-7">
@@ -32,48 +35,50 @@ export const SidebarSwitcher: FC<SidebarSwitcherProps> = ({
           contentType="chats"
           onContentTypeChange={onContentTypeChange}
         />
-
-        <SidebarSwitchItem
-          icon={<IconAdjustmentsHorizontal size={SIDEBAR_ICON_SIZE} />}
-          contentType="presets"
-          onContentTypeChange={onContentTypeChange}
-        />
-
-        <SidebarSwitchItem
-          icon={<IconPencil size={SIDEBAR_ICON_SIZE} />}
-          contentType="prompts"
-          onContentTypeChange={onContentTypeChange}
-        />
-
-        <SidebarSwitchItem
-          icon={<IconSparkles size={SIDEBAR_ICON_SIZE} />}
-          contentType="models"
-          onContentTypeChange={onContentTypeChange}
-        />
-
         <SidebarSwitchItem
           icon={<IconFile size={SIDEBAR_ICON_SIZE} />}
           contentType="files"
           onContentTypeChange={onContentTypeChange}
         />
 
-        <SidebarSwitchItem
-          icon={<IconBooks size={SIDEBAR_ICON_SIZE} />}
-          contentType="collections"
-          onContentTypeChange={onContentTypeChange}
-        />
+        {profile?.advanced_settings && (
+          <>
+            <SidebarSwitchItem
+              icon={<IconAdjustmentsHorizontal size={SIDEBAR_ICON_SIZE} />}
+              contentType="presets"
+              onContentTypeChange={onContentTypeChange}
+            />
 
-        <SidebarSwitchItem
-          icon={<IconRobotFace size={SIDEBAR_ICON_SIZE} />}
-          contentType="assistants"
-          onContentTypeChange={onContentTypeChange}
-        />
+            <SidebarSwitchItem
+              icon={<IconPencil size={SIDEBAR_ICON_SIZE} />}
+              contentType="prompts"
+              onContentTypeChange={onContentTypeChange}
+            />
 
-        <SidebarSwitchItem
-          icon={<IconBolt size={SIDEBAR_ICON_SIZE} />}
-          contentType="tools"
-          onContentTypeChange={onContentTypeChange}
-        />
+            <SidebarSwitchItem
+              icon={<IconSparkles size={SIDEBAR_ICON_SIZE} />}
+              contentType="models"
+              onContentTypeChange={onContentTypeChange}
+            />
+            <SidebarSwitchItem
+              icon={<IconBooks size={SIDEBAR_ICON_SIZE} />}
+              contentType="collections"
+              onContentTypeChange={onContentTypeChange}
+            />
+
+            <SidebarSwitchItem
+              icon={<IconRobotFace size={SIDEBAR_ICON_SIZE} />}
+              contentType="assistants"
+              onContentTypeChange={onContentTypeChange}
+            />
+
+            <SidebarSwitchItem
+              icon={<IconBolt size={SIDEBAR_ICON_SIZE} />}
+              contentType="tools"
+              onContentTypeChange={onContentTypeChange}
+            />
+          </>
+        )}
       </TabsList>
 
       <div className="flex flex-col items-center space-y-4">
--- components/utility/profile-settings.tsx
+++ components/utility/profile-settings.tsx
@@ -334,10 +334,12 @@ export const ProfileSettings: FC<ProfileSettingsProps> = ({}) => {
           </SheetHeader>
 
           <Tabs defaultValue="profile">
-            <TabsList className="mt-4 grid w-full grid-cols-2">
-              <TabsTrigger value="profile">Profile</TabsTrigger>
-              <TabsTrigger value="keys">API Keys</TabsTrigger>
-            </TabsList>
+            {profile?.advanced_settings && (
+              <TabsList className="mt-4 grid w-full grid-cols-2">
+                <TabsTrigger value="profile">Profile</TabsTrigger>
+                <TabsTrigger value="keys">API Keys</TabsTrigger>
+              </TabsList>
+            )}
 
             <TabsContent className="mt-4 space-y-4" value="profile">
               <div className="space-y-1">
