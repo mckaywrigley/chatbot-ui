5d7f7db1e30c65d59b108fda6078fc36b491e704
momonja <38156035+momonja3@users.noreply.github.com>
2024-02-02 04:11:09 +0900

    feat: Google Sign-In (#10)
    * feat: google sign-in
    
    * Add OR divider between authentication options
    
    ---------
    
    Co-authored-by: HackerGPT <contact@hackergpt.chat>

--- app/[locale]/login/page.tsx
+++ app/[locale]/login/page.tsx
@@ -9,6 +9,7 @@ import { Metadata } from "next"
 import { cookies, headers } from "next/headers"
 import { redirect } from "next/navigation"
 import { get } from "@vercel/edge-config"
+import { IconBrandGoogle } from "@tabler/icons-react"
 
 export const metadata: Metadata = {
   title: "Login"
@@ -138,61 +139,97 @@ export default async function Login({
     return redirect("/login?message=Check email to reset password")
   }
 
+  const handleSignInWithGoogle = async () => {
+    "use server"
+    const cookieStore = cookies()
+    const supabase = createClient(cookieStore)
+
+    const { error, data } = await supabase.auth.signInWithOAuth({
+      provider: "google"
+    })
+
+    console.log(data)
+    if (error) {
+      console.log(error)
+      return redirect(`/login?message=${error.message}`)
+    }
+
+    return redirect(data.url)
+  }
+
   return (
     <div className="flex w-full flex-1 flex-col justify-center gap-2 px-8 sm:max-w-md">
-      <form
-        className="animate-in text-foreground flex w-full flex-1 flex-col justify-center gap-2"
-        action={signIn}
-      >
-        <Brand />
+      <div>
+        <form className="animate-in text-foreground flex w-full flex-1 flex-col justify-center gap-2">
+          <Brand />
 
-        <Label className="text-md mt-4" htmlFor="email">
-          Email
-        </Label>
-        <Input
-          className="mb-3 rounded-md border bg-inherit px-4 py-2"
-          name="email"
-          placeholder="you@example.com"
-          required
-        />
-
-        <Label className="text-md" htmlFor="password">
-          Password
-        </Label>
-        <Input
-          className="mb-6 rounded-md border bg-inherit px-4 py-2"
-          type="password"
-          name="password"
-          placeholder="••••••••"
-        />
-
-        <Button className="mb-2 rounded-md bg-blue-700 px-4 py-2 text-white">
-          Login
-        </Button>
-
-        <Button
-          formAction={signUp}
-          className="border-foreground/20 mb-2 rounded-md border px-4 py-2"
-        >
-          Sign Up
-        </Button>
-
-        <div className="text-muted-foreground mt-1 flex justify-center text-sm">
-          <span className="mr-1">Forgot your password?</span>
-          <button
-            formAction={handleResetPassword}
-            className="text-primary ml-1 underline hover:opacity-80"
+          <Button
+            className="mt-4 rounded-md border border-gray-600 px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700"
+            variant="secondary"
+            role="button"
+            formAction={handleSignInWithGoogle}
           >
-            Reset
-          </button>
+            <IconBrandGoogle className="mr-1" size={20} />
+            Log in with Google
+          </Button>
+        </form>
+        <div className="mt-4 flex items-center">
+          <div className="grow border-t border-gray-300"></div>
+          <span className="mx-4 text-sm text-black dark:text-white">OR</span>
+          <div className="grow border-t border-gray-300"></div>
         </div>
+        <form
+          action={signIn}
+          className="animate-in text-foreground mt-4 flex w-full flex-1 flex-col justify-center gap-2"
+        >
+          <Label className="text-md mt-4" htmlFor="email">
+            Email
+          </Label>
+          <Input
+            className="mb-3 rounded-md border bg-inherit px-4 py-2"
+            name="email"
+            placeholder="you@example.com"
+            required
+          />
 
-        {searchParams?.message && (
-          <p className="bg-foreground/10 text-foreground mt-4 p-4 text-center">
-            {searchParams.message}
-          </p>
-        )}
-      </form>
+          <Label className="text-md" htmlFor="password">
+            Password
+          </Label>
+          <Input
+            className="mb-6 rounded-md border bg-inherit px-4 py-2"
+            type="password"
+            name="password"
+            placeholder="••••••••"
+          />
+
+          <Button className="mb-2 rounded-md bg-blue-700 px-4 py-2 text-white hover:bg-blue-800">
+            Login
+          </Button>
+
+          <Button
+            formAction={signUp}
+            className="border-foreground/20 mb-2 rounded-md border px-4 py-2 hover:border-gray-400"
+          >
+            Sign Up
+          </Button>
+
+          <div className="text-muted-foreground mt-1 flex justify-center text-sm">
+            <span className="mr-1">Forgot your password?</span>
+            <button
+              formAction={handleResetPassword}
+              className="text-primary ml-1 underline hover:opacity-80"
+            >
+              Reset
+            </button>
+          </div>
+
+          {searchParams?.message && (
+            <p className="bg-foreground/10 text-foreground mt-4 p-4 text-center">
+              {searchParams.message}
+            </p>
+          )}
+        </form>
+      </div>
     </div>
   )
 }
