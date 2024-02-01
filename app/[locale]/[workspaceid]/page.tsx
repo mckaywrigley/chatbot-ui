a9843487918146f4ee61ddab97ffd45c8baf23e4
Mckay Wrigley <mckaywrigley@gmail.com>
2024-02-01 15:44:58 -0700

    switcher works

---
+++ app/[locale]/[workspaceid]/page.tsx
@@ -0,0 +1,7 @@
+export default function WorkspacePage() {
+  return (
+    <div className="size-screen flex flex-col items-center justify-center">
+      <div className="text-4xl">Workspace Page</div>
+    </div>
+  )
+}
--- components/utility/global-state.tsx
+++ components/utility/global-state.tsx
@@ -2,20 +2,9 @@
 
 "use client"
 
-import { default as Loading } from "@/app/[locale]/loading"
 import { ChatbotUIContext } from "@/context/context"
-import { getAssistantWorkspacesByWorkspaceId } from "@/db/assistants"
-import { getChatsByWorkspaceId } from "@/db/chats"
-import { getCollectionWorkspacesByWorkspaceId } from "@/db/collections"
-import { getFileWorkspacesByWorkspaceId } from "@/db/files"
-import { getFoldersByWorkspaceId } from "@/db/folders"
-import { getModelWorkspacesByWorkspaceId } from "@/db/models"
-import { getPresetWorkspacesByWorkspaceId } from "@/db/presets"
 import { getProfileByUserId } from "@/db/profile"
-import { getPromptWorkspacesByWorkspaceId } from "@/db/prompts"
-import { getAssistantImageFromStorage } from "@/db/storage/assistant-images"
 import { getWorkspaceImageFromStorage } from "@/db/storage/workspace-images"
-import { getToolWorkspacesByWorkspaceId } from "@/db/tools"
 import { getWorkspacesByUserId } from "@/db/workspaces"
 import { convertBlobToBase64 } from "@/lib/blob-to-b64"
 import {
@@ -30,7 +19,6 @@ import {
   ChatMessage,
   ChatSettings,
   LLM,
-  LLMID,
   MessageImage,
   OpenRouterLLM,
   WorkspaceImage
@@ -132,16 +120,12 @@ export const GlobalState: FC<GlobalStateProps> = ({ children }) => {
   const [selectedTools, setSelectedTools] = useState<Tables<"tools">[]>([])
   const [toolInUse, setToolInUse] = useState<string>("none")
 
-  // THIS COMPONENT
-  const [loading, setLoading] = useState<boolean>(true)
-
   useEffect(() => {
     const fetchData = async () => {
       const profile = await fetchStartingData()
 
       if (profile) {
         const data = await fetchHostedModels(profile)
-
         if (!data) return
 
         setEnvKeyMap(data.envKeyMap)
@@ -159,38 +143,10 @@ export const GlobalState: FC<GlobalStateProps> = ({ children }) => {
         if (!localModels) return
         setAvailableLocalModels(localModels)
       }
-
-      // await fetchOpenaiAssistants()
     }
 
     fetchData()
   }, [])
-
-  useEffect(() => {
-    const isInChat = window?.location?.pathname === "/chat"
-
-    if (!selectedWorkspace && !isInChat) {
-      setLoading(false)
-      return
-    }
-
-    setUserInput("")
-    setChatMessages([])
-    setSelectedChat(null)
-
-    setIsGenerating(false)
-    setFirstTokenReceived(false)
-
-    setChatFiles([])
-    setChatImages([])
-    setNewMessageFiles([])
-    setNewMessageImages([])
-    setShowFilesDisplay(false)
-
-    if (selectedWorkspace?.id) {
-      fetchWorkspaceData(selectedWorkspace.id)
-    }
-  }, [selectedWorkspace])
 
   const fetchStartingData = async () => {
     const session = (await supabase.auth.getSession()).data.session
@@ -233,117 +189,8 @@ export const GlobalState: FC<GlobalStateProps> = ({ children }) => {
         }
       }
 
-      const homeWorkspace = workspaces.find(
-        workspace => workspace.is_home === true
-      )
-
-      // DB guarantees there will always be a home workspace
-      setSelectedWorkspace(homeWorkspace!)
-
-      setChatSettings({
-        model: (homeWorkspace?.default_model || "gpt-4-1106-preview") as LLMID,
-        prompt:
-          homeWorkspace?.default_prompt ||
-          "You are a friendly, helpful AI assistant.",
-        temperature: homeWorkspace?.default_temperature || 0.5,
-        contextLength: homeWorkspace?.default_context_length || 4096,
-        includeProfileContext: homeWorkspace?.include_profile_context || true,
-        includeWorkspaceInstructions:
-          homeWorkspace?.include_workspace_instructions || true,
-        embeddingsProvider:
-          (homeWorkspace?.embeddings_provider as "openai" | "local") || "openai"
-      })
-
       return profile
     }
-  }
-
-  const fetchWorkspaceData = async (workspaceId: string) => {
-    setLoading(true)
-
-    const assistantData = await getAssistantWorkspacesByWorkspaceId(workspaceId)
-    setAssistants(assistantData.assistants)
-
-    for (const assistant of assistantData.assistants) {
-      let url = ""
-
-      if (assistant.image_path) {
-        url = (await getAssistantImageFromStorage(assistant.image_path)) || ""
-      }
-
-      if (url) {
-        const response = await fetch(url)
-        const blob = await response.blob()
-        const base64 = await convertBlobToBase64(blob)
-
-        setAssistantImages(prev => [
-          ...prev,
-          {
-            assistantId: assistant.id,
-            path: assistant.image_path,
-            base64,
-            url
-          }
-        ])
-      } else {
-        setAssistantImages(prev => [
-          ...prev,
-          {
-            assistantId: assistant.id,
-            path: assistant.image_path,
-            base64: "",
-            url
-          }
-        ])
-      }
-    }
-
-    const chats = await getChatsByWorkspaceId(workspaceId)
-    setChats(chats)
-
-    const collectionData =
-      await getCollectionWorkspacesByWorkspaceId(workspaceId)
-    setCollections(collectionData.collections)
-
-    const folders = await getFoldersByWorkspaceId(workspaceId)
-    setFolders(folders)
-
-    const fileData = await getFileWorkspacesByWorkspaceId(workspaceId)
-    setFiles(fileData.files)
-
-    const presetData = await getPresetWorkspacesByWorkspaceId(workspaceId)
-    setPresets(presetData.presets)
-
-    const promptData = await getPromptWorkspacesByWorkspaceId(workspaceId)
-    setPrompts(promptData.prompts)
-
-    const toolData = await getToolWorkspacesByWorkspaceId(workspaceId)
-    setTools(toolData.tools)
-
-    const modelData = await getModelWorkspacesByWorkspaceId(workspaceId)
-    setModels(modelData.models)
-
-    setLoading(false)
-  }
-
-  const fetchOpenaiAssistants = async () => {
-    setLoading(true)
-
-    try {
-      const response = await fetch("/api/assistants/openai")
-
-      const data = await response.json()
-
-      setOpenaiAssistants(data.assistants)
-    } catch (error) {
-      console.warn("Error fetching OpenAI assistants: " + error)
-    }
-
-    setLoading(false)
-  }
-
-  if (loading) {
-    return <Loading />
   }
 
   return (
