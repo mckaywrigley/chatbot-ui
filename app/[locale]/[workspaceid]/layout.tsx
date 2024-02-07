66ab0027edc53d6ebc3be93f8c21f64f66afc226
thehackergpt <144740362+thehackergpt@users.noreply.github.com>
2024-02-07 09:40:45 -0500

    Set HackerGPT AI as default model in ChatSettings (#27)

--- app/[locale]/[workspaceid]/layout.tsx
+++ app/[locale]/[workspaceid]/layout.tsx
@@ -156,12 +156,12 @@ export default function WorkspaceLayout({ children }: WorkspaceLayoutProps) {
     setModels(modelData.models)
 
     setChatSettings({
-      model: (workspace?.default_model || "gpt-4-1106-preview") as LLMID,
+      model: "mistral-medium" as LLMID,
       prompt:
         workspace?.default_prompt ||
         "You are a friendly, helpful AI assistant.",
-      temperature: workspace?.default_temperature || 0.5,
-      contextLength: workspace?.default_context_length || 4096,
+      temperature: workspace?.default_temperature || 0.4,
+      contextLength: workspace?.default_context_length || 1024,
       includeProfileContext: workspace?.include_profile_context || true,
       includeWorkspaceInstructions:
         workspace?.include_workspace_instructions || true,
