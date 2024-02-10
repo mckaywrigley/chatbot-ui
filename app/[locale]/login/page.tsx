41ae2123252875328d594c811405c5bb4b685cb1
Bogdan Polonsky <polonsky@tochka.com>
2024-02-10 23:38:56 +0400

    feat: disable submitting buttons on login page

--- app/[locale]/login/page.tsx
+++ app/[locale]/login/page.tsx
@@ -1,7 +1,7 @@
 import { Brand } from "@/components/ui/brand"
-import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { SubmitButton } from "@/components/ui/submit-button"
 import { createClient } from "@/lib/supabase/server"
 import { Database } from "@/supabase/types"
 import { createServerClient } from "@supabase/ssr"
@@ -190,16 +190,16 @@ export default async function Login({
           placeholder="••••••••"
         />
 
-        <Button className="mb-2 rounded-md bg-blue-700 px-4 py-2 text-white">
+        <SubmitButton className="mb-2 rounded-md bg-blue-700 px-4 py-2 text-white">
           Login
-        </Button>
+        </SubmitButton>
 
-        <Button
+        <SubmitButton
           formAction={signUp}
           className="border-foreground/20 mb-2 rounded-md border px-4 py-2"
         >
           Sign Up
-        </Button>
+        </SubmitButton>
 
         <div className="text-muted-foreground mt-1 flex justify-center text-sm">
           <span className="mr-1">Forgot your password?</span>
