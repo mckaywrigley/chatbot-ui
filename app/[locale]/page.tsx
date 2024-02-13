fb160b5b9d20c1664fceb4da8520dd8070c8ae4d
spammenotinoz <spammenotinoz@gmail.com>
2024-02-13 21:46:48 +1100

    Update page.tsx

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -11,7 +11,7 @@ export default function HomePage() {
   return (
     <div className="flex size-full flex-col items-center justify-center">
 
-      <div className="mt-2 text-4xl font-bold">Ultimate UI</div>
+      <div className="mt-2 text-4xl font-bold">Ultimate AI</div>
 
       <Link
         className="mt-4 flex w-[200px] items-center justify-center rounded-md bg-blue-500 p-2 font-semibold"
