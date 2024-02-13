16a56e53182340f09469919e86f2a4244dcf127e
spammenotinoz <spammenotinoz@gmail.com>
2024-02-14 08:21:46 +1100

    WhiteBox

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -12,10 +12,10 @@ import { ReactNode } from "react"
 import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
-const APP_NAME = "Chatbot UI"
-const APP_DEFAULT_TITLE = "Chatbot UI"
-const APP_TITLE_TEMPLATE = "%s - Chatbot UI"
-const APP_DESCRIPTION = "Chabot UI PWA!"
+const APP_NAME = "Altimate AI"
+const APP_DEFAULT_TITLE = "Altimate AI"
+const APP_TITLE_TEMPLATE = "%s - Altimate AI"
+const APP_DESCRIPTION = "Altimate AI"
 
 interface RootLayoutProps {
   children: ReactNode
