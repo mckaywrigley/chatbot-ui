24342dfc72b934a2ac0e7c6e34127c59b8c94fe1
spammenotinoz <spammenotinoz@gmail.com>
2024-02-13 21:44:20 +1100

    Update page.tsx

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { ChatbotUISVG } from "@/components/icons/chatbotui-svg"
 import { IconArrowRight } from "@tabler/icons-react"
 import { useTheme } from "next-themes"
 import Link from "next/link"
@@ -9,9 +10,8 @@ export default function HomePage() {
 
   return (
     <div className="flex size-full flex-col items-center justify-center">
-      </div>
 
-      <div className="mt-2 text-4xl font-bold">Ultimate AI</div>
+      <div className="mt-2 text-4xl font-bold">Ultimate UI</div>
 
       <Link
         className="mt-4 flex w-[200px] items-center justify-center rounded-md bg-blue-500 p-2 font-semibold"
