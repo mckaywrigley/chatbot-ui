69642f806246a8813a91365985ee58814cf7ec13
ochen1 <o.chen1@share.epsb.ca>
2024-02-13 02:49:56 -0700

    Allow inactive chats to be renamed and deleted via sidebar buttons (#1425)

--- components/sidebar/items/chat/chat-item.tsx
+++ components/sidebar/items/chat/chat-item.tsx
@@ -57,7 +57,7 @@ export const ChatItem: FC<ChatItemProps> = ({ chat }) => {
     <div
       ref={itemRef}
       className={cn(
-        "hover:bg-accent focus:bg-accent flex w-full cursor-pointer items-center rounded p-2 hover:opacity-50 focus:outline-none",
+        "hover:bg-accent focus:bg-accent group flex w-full cursor-pointer items-center rounded p-2 hover:opacity-50 focus:outline-none",
         isActive && "bg-accent"
       )}
       tabIndex={0}
@@ -94,19 +94,17 @@ export const ChatItem: FC<ChatItemProps> = ({ chat }) => {
         {chat.name}
       </div>
 
-      {isActive && (
-        <div
-          onClick={e => {
-            e.stopPropagation()
-            e.preventDefault()
-          }}
-          className="ml-2 flex space-x-2"
-        >
-          <UpdateChat chat={chat} />
+      <div
+        onClick={e => {
+          e.stopPropagation()
+          e.preventDefault()
+        }}
+        className={`ml-2 flex space-x-2 ${!isActive && "w-11 opacity-0 group-hover:opacity-100"}`}
+      >
+        <UpdateChat chat={chat} />
 
-          <DeleteChat chat={chat} />
-        </div>
-      )}
+        <DeleteChat chat={chat} />
+      </div>
     </div>
   )
 }
