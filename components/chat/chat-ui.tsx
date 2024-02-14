ab93872eb5404384401b11f75f0f4d06bb020f8a
spammenotinoz <spammenotinoz@gmail.com>
2024-02-14 14:45:07 +1100

    Update chat-ui.tsx

--- components/chat/chat-ui.tsx
+++ components/chat/chat-ui.tsx
@@ -222,9 +222,6 @@ export const ChatUI: FC<ChatUIProps> = ({}) => {
         <ChatInput />
       </div>
 
-      <div className="absolute bottom-2 right-2 hidden md:block lg:bottom-4 lg:right-4">
-        <ChatHelp />
-      </div>
     </div>
   )
 }
