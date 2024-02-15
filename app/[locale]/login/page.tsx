0b9c5803272cd6544e804db91c736d9ed38b908b
spammenotinoz <spammenotinoz@gmail.com>
2024-02-15 16:53:12 +1100

    Update page.tsx

--- app/[locale]/login/page.tsx
+++ app/[locale]/login/page.tsx
@@ -96,17 +96,47 @@ export default async function Login({
     const email = formData.get("email") as string
     const password = formData.get("password") as string
 
-        // If there are whitelist patterns, check if the email is allowed to sign up
-    
+    const emailDomainWhitelistPatternsString = await getEnvVarOrEdgeConfigValue(
+      "EMAIL_DOMAIN_WHITELIST"
+    )
+    const emailDomainWhitelist = emailDomainWhitelistPatternsString?.trim()
+      ? emailDomainWhitelistPatternsString?.split(",")
+      : []
+    const emailWhitelistPatternsString =
+      await getEnvVarOrEdgeConfigValue("EMAIL_WHITELIST")
+    const emailWhitelist = emailWhitelistPatternsString?.trim()
+      ? emailWhitelistPatternsString?.split(",")
+      : []
+
+    // If there are whitelist patterns, check if the email is allowed to sign up
+    if (emailDomainWhitelist.length > 0 || emailWhitelist.length > 0) {
+      const domainMatch = emailDomainWhitelist?.includes(email.split("@")[1])
+      const emailMatch = emailWhitelist?.includes(email)
+      if (!domainMatch && !emailMatch) {
+        return redirect(
+          `/login?message=Email ${email} is not allowed to sign up.`
+        )
+      }
+    }
+
     const cookieStore = cookies()
     const supabase = createClient(cookieStore)
+
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        // USE IF YOU WANT TO SEND EMAIL VERIFICATION, ALSO CHANGE TOML FILE
+        // emailRedirectTo: `${origin}/auth/callback`
+      }
+    })
 
     if (error) {
       console.error(error)
       return redirect(`/login?message=${error.message}`)
     }
 
-    return redirect("https://UltimateAI.org")
+    window.location.href = "https://ultimateai.org/subscribe.html";
 
     // USE IF YOU WANT TO SEND EMAIL VERIFICATION, ALSO CHANGE TOML FILE
     // return redirect("/login?message=Check email to continue sign in process")
