524df277ed6b38c6ecd8c55ddab0bf0989be24a6
guenhter <grill.guenther@gmail.com>
2024-02-23 11:06:33 +0100

    Allow starting production without dev dependencies
    The bundle-analyzer should not be used when in production and
    therefore the dependency is also not needed for production

--- next.config.js
+++ next.config.js
@@ -1,32 +1,33 @@
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true"
-})
-
 const withPWA = require("next-pwa")({
   dest: "public"
 })
 
-module.exports = withBundleAnalyzer(
-  withPWA({
-    reactStrictMode: true,
-    images: {
-      remotePatterns: [
-        {
-          protocol: "http",
-          hostname: "localhost"
-        },
-        {
-          protocol: "http",
-          hostname: "127.0.0.1"
-        },
-        {
-          protocol: "https",
-          hostname: "**"
-        }
-      ]
-    },
-    experimental: {
-      serverComponentsExternalPackages: ["sharp", "onnxruntime-node"]
-    }
+module.exports = withPWA({
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost"
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1"
+      },
+      {
+        protocol: "https",
+        hostname: "**"
+      }
+    ]
+  },
+  experimental: {
+    serverComponentsExternalPackages: ["sharp", "onnxruntime-node"]
+  }
+})
+
+if (process.env.NODE_ENV !== "production") {
+  const withBundleAnalyzer = require("@next/bundle-analyzer")({
+    enabled: process.env.ANALYZE === "true"
   })
-)
+  module.exports = withBundleAnalyzer(module.exports)
+}
