de2ea8e65b102b9eb8f0ab2dee749bbce37ef28b
spammenotinoz <spammenotinoz@gmail.com>
2024-02-25 21:08:34 +1100

    Update page.tsx

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -21,5 +21,21 @@ export default function HomePage() {
         <IconArrowRight className="ml-1" size={20} />
       </Link>
     </div>
+	
+	<div className="flex size-full flex-col items-center justify-center">
+
+      <div className="mt-2 text-4xl font-bold">Ultimate AI</div>
+
+      <Link
+        className="mt-4 flex w-[200px] items-center justify-center rounded-md bg-blue-500 p-2 font-semibold"
+        href="https://mjw-spammenotinozs-projects.vercel.app/"
+      >
+        Midjourney
+        <IconArrowRight className="ml-1" size={20} />
+      </Link>
+    </div>
+	
+	
+	
   )
 }
