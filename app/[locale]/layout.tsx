ed18a2184a9e96427af1c7d64e771c855bddc953
PixelVerseIT <115069692+PixelVerseIT@users.noreply.github.com>
2024-03-10 20:56:48 +1030

    edited

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -12,10 +12,10 @@ import { ReactNode } from "react"
 import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
-const APP_NAME = "Chatbot UI"
-const APP_DEFAULT_TITLE = "Chatbot UI"
-const APP_TITLE_TEMPLATE = "%s - Chatbot UI"
-const APP_DESCRIPTION = "Chabot UI PWA!"
+const APP_NAME = "PixelVerseAI Labs Premium"
+const APP_DEFAULT_TITLE = "PixelVerseAI Labs Premium"
+const APP_TITLE_TEMPLATE = "%s - PXAI Labs"
+const APP_DESCRIPTION = "PixelVerseAI Labs Premium"
 
 interface RootLayoutProps {
   children: ReactNode
