b2242b7d194372f468df9d323d36695f8d873447
xycjscs <yuchao-xu@hotmail.com>
2024-03-11 20:27:46 +0800

    Solve the problem of displaying repeated incorrect words during the streaming output process in Chinese and other languages.

--- components/chat/chat-helpers/index.ts
+++ components/chat/chat-helpers/index.ts
@@ -325,7 +325,7 @@ export const processResponse = async (
               const updatedChatMessage: ChatMessage = {
                 message: {
                   ...chatMessage.message,
-                  content: chatMessage.message.content + contentToAdd
+                  content: fullText
                 },
                 fileItems: chatMessage.fileItems
               }
