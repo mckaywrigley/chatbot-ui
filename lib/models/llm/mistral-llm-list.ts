825375d18e871212ef520734bfe57c5d5c8a3252
PixelVerseIT <115069692+PixelVerseIT@users.noreply.github.com>
2024-03-19 18:58:24 +1030

    edited

--- lib/models/llm/mistral-llm-list.ts
+++ lib/models/llm/mistral-llm-list.ts
@@ -16,30 +16,30 @@ const MISTRAL_7B: LLM = {
 
 // Mixtral (UPDATED 12/21/23)
 const MIXTRAL: LLM = {
-  modelId: "mistral-small-latest",
+  modelId: "mistral-small",
   modelName: "Mistral Small",
   provider: "mistral",
-  hostedId: "mistral-small-latest",
+  hostedId: "mistral-small",
   platformLink: MISTRAL_PLATORM_LINK,
   imageInput: false
 }
 
 // Mistral Medium (UPDATED 12/21/23)
 const MISTRAL_MEDIUM: LLM = {
-  modelId: "mistral-medium-latest",
+  modelId: "mistral-medium",
   modelName: "Mistral Medium",
   provider: "mistral",
-  hostedId: "mistral-medium-latest",
+  hostedId: "mistral-medium",
   platformLink: MISTRAL_PLATORM_LINK,
   imageInput: false
 }
 
 // Mistral Large (UPDATED 03/05/24)
 const MISTRAL_LARGE: LLM = {
-  modelId: "mistral-large-latest",
+  modelId: "mistral-large-2402",
   modelName: "Mistral Large",
   provider: "mistral",
-  hostedId: "mistral-large-latest",
+  hostedId: "mistral-large-2402",
   platformLink: MISTRAL_PLATORM_LINK,
   imageInput: false
 }
