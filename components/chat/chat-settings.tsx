937739fb8881cec07cd75179ffce91b457e16335
Mckay Wrigley <mckaywrigley@gmail.com>
2024-03-22 21:24:16 -0600

    style fix

--- components/chat/chat-settings.tsx
+++ components/chat/chat-settings.tsx
@@ -72,7 +72,7 @@ export const ChatSettings: FC<ChatSettingsProps> = ({}) => {
           className="flex items-center space-x-2"
           variant="ghost"
         >
-          <div className="w-[120px] truncate text-lg sm:w-[300px] lg:w-[500px]">
+          <div className="max-w-[120px] truncate text-lg sm:max-w-[300px] lg:max-w-[500px]">
             {fullModel?.modelName || chatSettings.model}
           </div>
 
