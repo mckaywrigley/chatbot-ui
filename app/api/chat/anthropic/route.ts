bccae301a9a6920604082a5987e0d5d06d043a5c
PixelVerseIT <115069692+PixelVerseIT@users.noreply.github.com>
2024-03-23 15:26:26 +1030

    Added claude image support

--- app/api/chat/anthropic/route.ts
+++ app/api/chat/anthropic/route.ts
@@ -1,12 +1,14 @@
 import { CHAT_SETTING_LIMITS } from "@/lib/chat-setting-limits"
 import { checkApiKey, getServerProfile } from "@/lib/server/server-chat-helpers"
+import { getBase64FromDataURL, getMediaTypeFromDataURL } from "@/lib/utils"
 import { ChatSettings } from "@/types"
 import Anthropic from "@anthropic-ai/sdk"
 import { AnthropicStream, StreamingTextResponse } from "ai"
+import { NextRequest, NextResponse } from "next/server"
 
-// export const runtime = "edge"
+export const runtime = "edge"
 
-export async function POST(request: Request) {
+export async function POST(request: NextRequest) {
   const json = await request.json()
   const { chatSettings, messages } = json as {
     chatSettings: ChatSettings
@@ -20,23 +22,76 @@ export async function POST(request: Request) {
 
     let ANTHROPIC_FORMATTED_MESSAGES: any = messages.slice(1)
 
+    ANTHROPIC_FORMATTED_MESSAGES = ANTHROPIC_FORMATTED_MESSAGES?.map(
+      (message: any) => {
+        const messageContent =
+          typeof message?.content === "string"
+            ? [message.content]
+            : message?.content
+
+        return {
+          ...message,
+          content: messageContent.map((content: any) => {
+            if (typeof content === "string") {
+              // Handle the case where content is a string
+              return { type: "text", text: content }
+            } else if (
+              content?.type === "image_url" &&
+              content?.image_url?.length
+            ) {
+              return {
+                type: "image",
+                source: {
+                  type: "base64",
+                  media_type: getMediaTypeFromDataURL(content.image_url),
+                  data: getBase64FromDataURL(content.image_url)
+                }
+              }
+            } else {
+              return content
+            }
+          })
+        }
+      }
+    )
+
     const anthropic = new Anthropic({
       apiKey: profile.anthropic_api_key || ""
     })
 
-    const response = await anthropic.messages.create({
-      model: chatSettings.model,
-      messages: ANTHROPIC_FORMATTED_MESSAGES,
-      temperature: chatSettings.temperature,
-      system: messages[0].content,
-      max_tokens:
-        CHAT_SETTING_LIMITS[chatSettings.model].MAX_TOKEN_OUTPUT_LENGTH,
-      stream: true
-    })
+    try {
+      const response = await anthropic.messages.create({
+        model: chatSettings.model,
+        messages: ANTHROPIC_FORMATTED_MESSAGES,
+        temperature: chatSettings.temperature,
+        system: messages[0].content,
+        max_tokens:
+          CHAT_SETTING_LIMITS[chatSettings.model].MAX_TOKEN_OUTPUT_LENGTH,
+        stream: true
+      })
 
-    const stream = AnthropicStream(response)
-
-    return new StreamingTextResponse(stream)
+      try {
+        const stream = AnthropicStream(response)
+        return new StreamingTextResponse(stream)
+      } catch (error: any) {
+        console.error("Error parsing Anthropic API response:", error)
+        return new NextResponse(
+          JSON.stringify({
+            message:
+              "An error occurred while parsing the Anthropic API response"
+          }),
+          { status: 500 }
+        )
+      }
+    } catch (error: any) {
+      console.error("Error calling Anthropic API:", error)
+      return new NextResponse(
+        JSON.stringify({
+          message: "An error occurred while calling the Anthropic API"
+        }),
+        { status: 500 }
+      )
+    }
   } catch (error: any) {
     let errorMessage = error.message || "An unexpected error occurred"
     const errorCode = error.status || 500
@@ -49,7 +104,7 @@ export async function POST(request: Request) {
         "Anthropic API Key is incorrect. Please fix it in your profile settings."
     }
 
-    return new Response(JSON.stringify({ message: errorMessage }), {
+    return new NextResponse(JSON.stringify({ message: errorMessage }), {
       status: errorCode
     })
   }
