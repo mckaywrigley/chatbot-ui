a60ee29cef9f38a2853199ae39dfd7f1f5abec02
= <gerosullivan@gmail.com>
2024-03-26 09:39:26 +0000

    Remove unused code in QuickResponse component

--- components/chat/QuickResponse.tsx
+++ components/chat/QuickResponse.tsx
@@ -42,18 +42,3 @@ const QuickResponse = () => {
 }
 
 export default QuickResponse
-
-// <div className="absolute bottom-full left-0 right-0">
-//   <div className="relative h-full w-full">
-//     <div className="h-full flex ml-1 md:w-full md:m-auto md:mb-4 gap-0 md:gap-2 justify-center">
-//       <div className="grow"><div className="absolute bottom-full left-0 mb-4 flex w-full grow gap-2 px-1 pb-1 sm:px-2 sm:pb-0 md:static md:mb-0 md:max-w-none">
-//         <div className="grid w-full grid-flow-row grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-2">
-//         <div className="flex flex-col gap-2">
-//           </div>
-//           <div className="flex flex-col gap-2">
-//           </div>
-//         </div>
-//       </div></div>
-//     </div>
-//   </div>
-// </div>
