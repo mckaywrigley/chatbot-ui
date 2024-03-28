762eb10e5f7cbb9947ebdbe4c9ac734dad4dbb2a
tao <94625948+qte123@users.noreply.github.com>
2024-03-29 03:53:05 +0800

    Update server-chat-helpers.ts

--- lib/server/server-chat-helpers.ts
+++ lib/server/server-chat-helpers.ts
@@ -52,7 +52,7 @@ function addApiKeysToProfile(profile: Tables<"profiles">) {
 
     [VALID_ENV_KEYS.AZURE_OPENAI_ENDPOINT]: "azure_openai_endpoint",
     [VALID_ENV_KEYS.AZURE_GPT_35_TURBO_NAME]: "azure_openai_35_turbo_id",
-    [VALID_ENV_KEYS.AZURE_GPT_45_VISION_NAME]: "azure_openai_45vision_id",
+    [VALID_ENV_KEYS.AZURE_GPT_45_VISION_NAME]: "azure_openai_45_vision_id",
     [VALID_ENV_KEYS.AZURE_GPT_45_TURBO_NAME]: "azure_openai_45_turbo_id",
     [VALID_ENV_KEYS.AZURE_EMBEDDINGS_NAME]: "azure_openai_embeddings_id"
   }
