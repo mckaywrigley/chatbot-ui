10e582d3ef633da235c6a33d64f0b8c1c9fbfab3
sathish jagana <120253584+Sathishjagana123@users.noreply.github.com>
2024-04-12 11:58:52 +0530

    Create Provider.tf

---
+++ Eks-terraform/Provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
