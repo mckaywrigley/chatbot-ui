9cbd4da133a375bdfc6722a3fe165fde6785c375
Alain Picard <apicard@benchmarkconsulting.com>
2024-04-15 05:53:12 -0400

    Fix error returning error message and improve user message

--- app/api/retrieval/process/route.ts
+++ app/api/retrieval/process/route.ts
@@ -59,11 +59,16 @@ export async function POST(req: Request) {
     const fileExtension = fileMetadata.name.split(".").pop()?.toLowerCase()
 
     if (embeddingsProvider === "openai") {
-      if (profile.use_azure_openai) {
-        checkApiKey(profile.azure_openai_api_key, "Azure OpenAI")
-      } else {
-        checkApiKey(profile.openai_api_key, "OpenAI")
-      }
+      try {
+        if (profile.use_azure_openai) {
+          checkApiKey(profile.azure_openai_api_key, "Azure OpenAI")
+        } else {
+          checkApiKey(profile.openai_api_key, "OpenAI")
+        }
+      } catch (error: any) {
+        error.message = error.message + ", make sure it is configured or else use local embeddings"
+        throw(error)
+      }        
     }
 
     let chunks: FileItemChunk[] = []
@@ -158,7 +163,8 @@ export async function POST(req: Request) {
       status: 200
     })
   } catch (error: any) {
-    const errorMessage = error.error?.message || "An unexpected error occurred"
+    console.log(`Error in retrieval/process: ${error.stack}`)
+    const errorMessage = error?.message || "An unexpected error occurred"
     const errorCode = error.status || 500
     return new Response(JSON.stringify({ message: errorMessage }), {
       status: errorCode
