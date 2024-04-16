abc4c2a55918d1540994eb02a0a7ae4334349bb6
Finn <37416552+fbec76@users.noreply.github.com>
2024-04-16 10:04:26 +0000

    Add gpt-4-turbo #1659

--- lib/chat-setting-limits.ts
+++ lib/chat-setting-limits.ts
@@ -102,7 +102,7 @@ export const CHAT_SETTING_LIMITS: Record<LLMID, ChatSettingLimits> = {
     MAX_CONTEXT_LENGTH: 4096
     // MAX_CONTEXT_LENGTH: 16385 (TODO: Change this back to 16385 when OpenAI bumps the model)
   },
-  "gpt-4-turbo-preview": {
+  "gpt-4-turbo": {
     MIN_TEMPERATURE: 0.0,
     MAX_TEMPERATURE: 2.0,
     MAX_TOKEN_OUTPUT_LENGTH: 4096,
