273311fdb6e0df5a58effcf98c35d2a4332a2b83
thehackergpt <144740362+thehackergpt@users.noreply.github.com>
2024-04-19 17:35:26 -0400

    Fix error returning error message and improve user message (#219)

--- db/files.ts
+++ db/files.ts
@@ -155,7 +155,14 @@ export const createFile = async (
   })
 
   if (!response.ok) {
-    toast.error("Failed to process file.")
+    const jsonText = await response.text()
+    const json = JSON.parse(jsonText)
+    console.error(
+      `Error processing file:${createdFile.id}, status:${response.status}, response:${json.message}`
+    )
+    toast.error("Failed to process file. Reason:" + json.message, {
+      duration: 10000
+    })
     await deleteFile(createdFile.id)
   }
 
@@ -220,6 +227,14 @@ export const createDocXFile = async (
 
   if (!response.ok) {
     toast.error("Failed to process file.")
+    const jsonText = await response.text()
+    const json = JSON.parse(jsonText)
+    console.error(
+      `Error processing file:${createdFile.id}, status:${response.status}, response:${json.message}`
+    )
+    toast.error("Failed to process file. Reason:" + json.message, {
+      duration: 10000
+    })
     await deleteFile(createdFile.id)
   }
 
