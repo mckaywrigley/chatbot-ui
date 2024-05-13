f21121c7b23912fcce48f0523b7c2d32bb6afd86
= <gerosullivan@gmail.com>
2024-05-13 15:30:23 +0100

    change default theme to light

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -90,7 +90,7 @@ export default async function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <Providers attribute="class" defaultTheme="dark">
+        <Providers attribute="class" defaultTheme="light">
           <TranslationsProvider
             namespaces={i18nNamespaces}
             locale={locale}
