561b44e9214d39cf0ec7263108188eb6fd824344
Rostyslav Manko <144740362+thehackergpt@users.noreply.github.com>
2024-05-20 10:45:23 -0400

    Fix mobile ui zooming problem (#314)

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -109,6 +109,12 @@ export default async function RootLayout({
 
   return (
     <html lang="en" suppressHydrationWarning className="h-full">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </head>
       <body className={inter.className + " h-full"}>
         <Providers attribute="class" defaultTheme="dark">
           <TranslationsProvider
