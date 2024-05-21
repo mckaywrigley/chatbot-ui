20fb8adaf857b5085a1ec2cf9f813e20abece65b
h-nakata <38676672+h-nakata@users.noreply.github.com>
2024-05-21 17:08:01 +0900

    custom miibo connect

--- app/api/chat/custom/route.ts
+++ app/api/chat/custom/route.ts
@@ -32,21 +32,75 @@ export async function POST(request: Request) {
       throw new Error(error.message)
     }
 
-    const custom = new OpenAI({
-      apiKey: customModel.api_key || "",
-      baseURL: customModel.base_url
-    })
+    if (customModel.name === "miibo") {
+      const response = await fetch(customModel.base_url, {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          api_key: customModel.api_key || "",
+          agent_id: customModel.model_id,
+          utterance: messages[messages.length - 1]["content"],
+          stream: true
+        })
+      })
+      const reader = response.body?.getReader()
+      const decoder = new TextDecoder()
+      if (!reader) return
 
-    const response = await custom.chat.completions.create({
-      model: chatSettings.model as ChatCompletionCreateParamsBase["model"],
-      messages: messages as ChatCompletionCreateParamsBase["messages"],
-      temperature: chatSettings.temperature,
-      stream: true
-    })
+      let newStrIndex = 0
+      const stream = new ReadableStream({
+        async start(controller) {
+          while (true) {
+            const { done, value } = await reader.read()
+            if (done) {
+              controller.close()
+              return
+            }
 
-    const stream = OpenAIStream(response)
+            const dataString = decoder.decode(value).split("\n").filter(x => x != "")
+            const responseData = dataString[dataString.length - 1]
 
-    return new StreamingTextResponse(stream)
+            const encoder = new TextEncoder()
+            if (responseData) {
+              try {
+                const jsonData = JSON.parse(responseData)
+                const responseLines = jsonData.bestResponse.utterance
+                const buffer = responseLines.slice(newStrIndex)
+
+                const queue = encoder.encode(buffer)
+                controller.enqueue(queue)
+
+                const segmenter = new Intl.Segmenter("ja", {granularity: "grapheme"})
+                const segments = segmenter.segment(responseLines)
+                newStrIndex = [...segments].length
+              } catch (e) {
+                console.error("JSON Analysis Error:", e)
+              }
+            }
+          }
+        }
+      })
+
+      return new Response(stream, {
+        headers: { "Content-Type": "text/event-stream; charset=utf-8" },
+      })
+    } else {
+      const custom = new OpenAI({
+        apiKey: customModel.api_key || "",
+        baseURL: customModel.base_url
+      })
+
+      const response = await custom.chat.completions.create({
+        model: chatSettings.model as ChatCompletionCreateParamsBase["model"],
+        messages: messages as ChatCompletionCreateParamsBase["messages"],
+        temperature: chatSettings.temperature,
+        stream: true
+      })
+
+      const stream = OpenAIStream(response)
+
+      return new StreamingTextResponse(stream)
+    }
   } catch (error: any) {
     let errorMessage = error.message || "An unexpected error occurred"
     const errorCode = error.status || 500
