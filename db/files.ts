43d56bb18200542cff60d6c3630dfa2251e71ed8
mikeysmet <26899585+Mikey032@users.noreply.github.com>
2024-05-29 12:55:04 +0200

    Only strip suffix of file if an extension is added

--- db/files.ts
+++ db/files.ts
@@ -93,7 +93,8 @@ export const createFile = async (
 ) => {
   let validFilename = fileRecord.name.replace(/[^a-z0-9.]/gi, "_").toLowerCase()
   const extension = file.name.split(".").pop()
-  const baseName = validFilename.substring(0, validFilename.lastIndexOf("."))
+  const extensionIndex = validFilename.lastIndexOf(".")
+  const baseName = validFilename.substring(0, (extensionIndex < 0) ? undefined : extensionIndex)
   const maxBaseNameLength = 100 - (extension?.length || 0) - 1
   if (baseName.length > maxBaseNameLength) {
     fileRecord.name = baseName.substring(0, maxBaseNameLength) + "." + extension
