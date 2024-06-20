b81629681cc6e5770ab77860ffc3d2e937c3b98e
Francisco Moretti <franciscoemoretti@gmail.com>
2024-06-20 10:23:08 +0100

    bugfix key can be mayus when shift pressed

--- lib/hooks/use-hotkey.tsx
+++ lib/hooks/use-hotkey.tsx
@@ -3,7 +3,11 @@ import { useEffect } from "react"
 const useHotkey = (key: string, callback: () => void): void => {
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent): void => {
-      if (event.metaKey && event.shiftKey && event.key === key) {
+      if (
+        event.metaKey &&
+        event.shiftKey &&
+        event.key.toLowerCase() === key.toLowerCase()
+      ) {
         event.preventDefault()
         callback()
       }
