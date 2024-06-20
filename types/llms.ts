5476d44f9a99ddca2f2b051e2cc2eeb10a1b59a7
Meet Patel <113581962+meetpateltech@users.noreply.github.com>
2024-06-20 20:40:14 +0530

    add anthropic sonnet 3.5 model

--- types/llms.ts
+++ types/llms.ts
@@ -30,6 +30,7 @@ export type AnthropicLLMID =
   | "claude-3-haiku-20240307" // Claude 3 Haiku
   | "claude-3-sonnet-20240229" // Claude 3 Sonnet
   | "claude-3-opus-20240229" // Claude 3 Opus
+  | "claude-3-5-sonnet-20240620" // Claude 3.5 Sonnet
 
 // Mistral Models
 export type MistralLLMID =
