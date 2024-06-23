da9858bb41d850c32aca0972ab24eeaf2a20bbb0
shray sharma <sharma.shray92@Gmail.com>
2024-06-23 21:45:20 +0200

    Add files via upload

--- components/chat/chat-help.tsx
+++ components/chat/chat-help.tsx
@@ -27,7 +27,7 @@ export const ChatHelp: FC<ChatHelpProps> = ({}) => {
   return (
     <DropdownMenu open={isOpen} onOpenChange={setIsOpen}>
       <DropdownMenuTrigger asChild>
-        <IconQuestionMark className="bg-primary text-secondary size-[24px] cursor-pointer rounded-full p-0.5 opacity-60 hover:opacity-50 lg:size-[30px] lg:p-1" />
+        <IconQuestionMark  id="icon-question-mark" className="bg-primary text-secondary size-[24px] cursor-pointer rounded-full p-0.5 opacity-60 hover:opacity-50 lg:size-[30px] lg:p-1" />
       </DropdownMenuTrigger>
 
       <DropdownMenuContent align="end">
